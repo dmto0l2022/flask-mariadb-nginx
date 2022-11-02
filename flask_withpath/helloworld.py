708cb5778eb3e68204d221087c6220d9a11c6f8e
andrewcgaitskell <andrew@gaitskell.com>
2022-11-02 14:56:45 +0000

    Create helloworld.py

---
+++ flask_withpath/helloworld.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+##app.config['APPLICATION_ROOT'] = '/flask1'
+api = Api(app, prefix='/flask1')
+
+class Greeting (Resource):
+    def get(self):
+        return 'Hello World!'
+
+api.add_resource(Greeting, '/') # Route_1
+
+if __name__ == '__main__':
+    app.run('0.0.0.0','5000')
