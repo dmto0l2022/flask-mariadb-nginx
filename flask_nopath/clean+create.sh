63d57e3df009d5fa966df4927d03ba9a9df9e936
andrewcgaitskell <andrew@gaitskell.com>
2022-11-02 15:58:58 +0000

    Update clean+create.sh

--- flask_nopath/clean+create.sh
+++ flask_nopath/clean+create.sh
@@ -3,8 +3,8 @@ podman pod rm frontend
 podman rmi -a
 cd /home/andrew_gaitskell/project/flask-mariadb-nginx/flask_nopath
 podman build -t my-flask-1 .
-cd /home/andrew_gaitskell/project/flask-mariadb-nginx/nginxwithindex
-podman build -t my-nginxwithindex-1 .
-podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginxwithindex-1:latest
+cd /home/andrew_gaitskell/project/flask-mariadb-nginx/nginx
+podman build -t my-nginx-1 .
+podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx-1:latest
 podman run -dt --pod frontend localhost/my-flask-1:latest
 podman pod start frontend
