e8d9d08123c3dbdd05d4fbdd2f8cf08fdf86fa61
andrewcgaitskell <andrew@gaitskell.com>
2022-11-02 15:06:00 +0000

    Update helloworld.py

--- flask_withpath/helloworld.py
+++ flask_withpath/helloworld.py
@@ -1,16 +1,26 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+#from flask_restful import Resource, Api
 
-app = Flask(__name__)
-api = Api(app)
+#app = Flask(__name__)
+#api = Api(app)
 ##app.config['APPLICATION_ROOT'] = '/flask1'
-api = Api(app, prefix='/flask1')
+#api = Api(app, prefix='/flask1')
 
-class Greeting (Resource):
-    def get(self):
-        return 'Hello World!'
+#class Greeting (Resource):
+#    def get(self):
+#        return 'Hello World!'
 
-api.add_resource(Greeting, '/') # Route_1
+#api.add_resource(Greeting, '/') # Route_1
+
+#if __name__ == '__main__':
+#    app.run('0.0.0.0','5000')
+
+from flask import Flask
+app = Flask(__name__)
+##CORS(app) #Prevents CORS errors 
+@app.route(‘/’)
+def index():
+    return “Hello, World!”
 
 if __name__ == '__main__':
-    app.run('0.0.0.0','5000')
+    app.run(debug=True, host = '0.0.0.0', port = 5000)
