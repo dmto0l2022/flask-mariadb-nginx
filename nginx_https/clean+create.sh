c2fff255b96f0aab8be066599b05b9986a0cac04
andrewcgaitskell <andrew@gaitskell.com>
2022-11-03 10:27:10 +0000

    Update clean+create.sh

--- nginx_https/clean+create.sh
+++ nginx_https/clean+create.sh
@@ -4,4 +4,8 @@ podman rmi -a
 cd /home/andrew_gaitskell/project/flask-mariadb-nginx/nginx_https
 podman build -t my-nginx-1 .
 podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx-1:latest
+
+podman run -d -p 8080:8000 --name webapp1 -v /opt/data:/opt oraclelinux:pyhttp
+
+
 podman pod start frontend
