4875ad185c74e2cde7d0830e193648366c8fab5e
andrewcgaitskell <andrew@gaitskell.com>
2022-11-04 09:58:22 +0000

    Update app.py

--- flask_dance/app.py
+++ flask_dance/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.github import make_github_blueprint, github
 
 app = Flask(__name__)
-api = Api(app)
+app.secret_key = "supersekrit"
+blueprint = make_github_blueprint(
+    client_id="my-key-here",
+    client_secret="my-secret-here",
+)
+app.register_blueprint(blueprint, url_prefix="/login")
 
-class Greeting (Resource):
-    def get(self):
-        return 'Hello World!'
-
-api.add_resource(Greeting, '/') # Route_1
+@app.route("/")
+def index():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok
+    return "You are @{login} on GitHub".format(login=resp.json()["login"])
 
 if __name__ == '__main__':
     app.run('0.0.0.0','5000')
