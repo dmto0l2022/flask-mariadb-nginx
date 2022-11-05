d5052f03524a9a2d85a62e0c6e77a726f6f174aa
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 18:43:24 +0000

    Update b2b.py

--- flask_dance_sqla/b2b.py
+++ flask_dance_sqla/b2b.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #"mariadb+mariadbconnector://" + MARIADB_USERNAME + ":" + MARIADB_PASSWORD + "@localhost:3306/" + MARIADB_DATABASE
 
-engine_string = "mariadb+mariadbconnector://pythonuser:pythonuser@localhost:3306/mysql"
+engine_string = "mariadb+mariadbconnector://pythonuser:pythonuser@mariadb1:3306/mysql"
 
 
 #engine = create_engine("mysql+pymysql://sylvain:passwd@localhost/db",
