013574bd78ca2d35b326afadad19d3c967c558fa
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 17:25:02 +0000

    Update simple.sh

--- mariadb/simple.sh
+++ mariadb/simple.sh
@@ -1,2 +1,6 @@
- podman run -p 3306:3306 -d --name mariadb -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4
+podman pod stop frontend
+podman pod rm frontend
+podman rmi -a
+ 
+podman run -p 3306:3306 -d --name mariadb -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4
  
