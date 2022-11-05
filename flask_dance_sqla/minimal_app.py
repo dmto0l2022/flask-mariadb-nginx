d6300fe48eb86a0cf74456d2da6e6604071e7b1c
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 10:36:37 +0000

    Update minimal_app.py

--- flask_dance_sqla/minimal_app.py
+++ flask_dance_sqla/minimal_app.py
@@ -23,7 +23,7 @@ load_dotenv(path.join(BASE_DIR, ".env"))
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import mariadb
 
 ##engine = sqlalchemy.create_engine("mariadb+mariadbconnector://app_user:Password123!@127.0.0.1:3306/company")
 
@@ -31,7 +31,9 @@ MARIADB_USERNAME = environ.get("MARIADB_USERNAME")
 MARIADB_PASSWORD = environ.get("MARIADB_PASSWORD")
 MARIADB_DATABASE = environ.get("MARIADB_DATABASE")
 
-MARIADB_URI = "mariadb+mariadbconnector://" + MARIADB_USERNAME + ":" + MARIADB_PASSWORD + "@127.0.0.1:3306/" + MARIADB_DATABASE
+#MARIADB_URI = "mariadb+mariadbconnector://" + MARIADB_USERNAME + ":" + MARIADB_PASSWORD + "@127.0.0.1:3306/" + MARIADB_DATABASE
+
+MARIADB_URI = "mariadb://" + MARIADB_USERNAME + ":" + MARIADB_PASSWORD + "@127.0.0.1:3306/" + MARIADB_DATABASE
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
