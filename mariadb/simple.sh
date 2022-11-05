38a63cd75ff67f719ed9f3a7323410949a35868c
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 17:51:59 +0000

    Update simple.sh

--- mariadb/simple.sh
+++ mariadb/simple.sh
@@ -5,6 +5,19 @@ podman rmi -a
 ## docker run -p 3306:3306 -d --name mariadb -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4 
 ## podman run -dt --pod new:frontend -p 3306:3306 localhost/my-mariadb-1:latest
 
-podman run -dt --pod new:frontend -p 3306:3306 -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4
+podman pod create \
+--name frontend \
+--publish 3306:3306
+
+podman run --detach \
+--pod frontend \
+--restart=always \
+-e MARIADB_ROOT_PASSWORD="badpassword0" \
+-e MARIADB_DATABASE="wp_db" \
+-e MYSQL_USER="tux" \
+-e MYSQL_PASSWORD="badpassword1" \
+--name=mariadb1 mariadb
+
+##podman run -dt --pod new:frontend -p 3306:3306 -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4
 
 podman pod start frontend
