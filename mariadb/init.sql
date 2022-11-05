607886be97be83b50e6e04472aa4f63446f21829
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 10:24:03 +0000

    Update init.sql

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -1,5 +1,7 @@
 CREATE USER 'pythonuser'@'localhost' IDENTIFIED BY 'pythonuser';
 
+CREATE DATABASE dmtools;
+
 GRANT ALL PRIVILEGES ON *.* TO 'pythonuser'@'localhost' WITH GRANT OPTION;
 
 ALTER USER 'pythonuser'@'%' IDENTIFIED BY 'pythonuser';
