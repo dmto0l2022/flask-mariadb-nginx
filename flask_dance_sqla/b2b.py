62a3c283e9831f8c98f1d11330c5c5abad79c7a2
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 12:49:33 +0000

    Update b2b.py

--- flask_dance_sqla/b2b.py
+++ flask_dance_sqla/b2b.py
@@ -1,3 +1,10 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://scott:tiger@localhost:5432/mydatabase")
+#engine = create_engine("postgresql+psycopg2://scott:tiger@localhost:5432/mydatabase")
+
+#"mariadb+mariadbconnector://" + MARIADB_USERNAME + ":" + MARIADB_PASSWORD + "@localhost:3306/" + MARIADB_DATABASE
+
+engine_string = "mariadb+mariadbconnector://pythonuser:pythonuser@localhost:3306/world"
+
+engine = create_engine(engine_string)
+
