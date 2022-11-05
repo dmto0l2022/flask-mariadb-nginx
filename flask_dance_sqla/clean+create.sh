2b5d362963a5a174f585e3f824c7bba255aac43a
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 18:32:19 +0000

    Update clean+create.sh

--- flask_dance_sqla/clean+create.sh
+++ flask_dance_sqla/clean+create.sh
@@ -2,10 +2,6 @@ podman pod stop frontend
 podman pod rm frontend
 podman rmi -a
 
-podman pod stop frontend
-podman pod rm frontend
-podman rmi -a
-
 ## docker run -p 3306:3306 -d --name mariadb -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4 
 ## podman run -dt --pod new:frontend -p 3306:3306 localhost/my-mariadb-1:latest
 
