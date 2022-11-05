53f1ae1ece082b45c28416b1131d17a2817884ba
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 19:01:40 +0000

    Update clean+create.sh

--- flask_dance_sqla/clean+create.sh
+++ flask_dance_sqla/clean+create.sh
@@ -14,17 +14,23 @@ podman pod create \
 #env MARIADB_ROOT_PASSWORD=pythonuser
 #env MARIADB_DATABASE=world
 
-podman run --detach \
---pod frontend \
---restart=always \
--e MARIADB_ROOT_PASSWORD="pythonuser" \
--e MARIADB_DATABASE="world" \
--e MARIADB_USER="pythonuser" \
--e MARIADB_PASSWORD="pythonuser" \
--e MARIADB_ROOT_HOST="localhost" \
---name=mariadb1 mariadb
+#podman run --detach \
+#--pod frontend \
+#--restart=always \
+#-e MARIADB_ROOT_PASSWORD="pythonuser" \
+#-e MARIADB_DATABASE="world" \
+#-e MARIADB_USER="pythonuser" \
+#-e MARIADB_PASSWORD="pythonuser" \
+#-e MARIADB_ROOT_HOST="localhost" \
+#--name=mariadb1 mariadb
 
 ##podman run -dt --pod new:frontend -p 3306:3306 -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4
+
+cd /home/andrew_gaitskell/project/flask-mariadb-nginx/mariadb
+podman build -t my-mariadb-1:latest .
+podman run -dt \
+--pod frontend localhost/my-mariadb-1:latest \
+--name=mariadb1 mariadb
 
 cd /home/andrew_gaitskell/project/flask-mariadb-nginx/flask_dance_sqla
 podman build -t my-flaskdancesqla-1 .
