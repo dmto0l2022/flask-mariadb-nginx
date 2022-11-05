02a96c519bb548bee3b7b117180c29c430fc63aa
andrewcgaitskell <andrew@gaitskell.com>
2022-11-05 17:30:07 +0000

    Update simple.sh

--- mariadb/simple.sh
+++ mariadb/simple.sh
@@ -3,11 +3,8 @@ podman pod rm frontend
 podman rmi -a
 
 ## docker run -p 3306:3306 -d --name mariadb -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4 
-## 
+## podman run -dt --pod new:frontend -p 3306:3306 localhost/my-mariadb-1:latest
 
-podman run -dt --pod new:frontend -p 3306:3306 -d --name mariadb -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4
- 
-cd /home/andrew_gaitskell/project/flask-mariadb-nginx/mariadb
-podman build -t my-mariadb-1 .
-podman run -dt --pod new:frontend -p 3306:3306 localhost/my-mariadb-1:latest
+podman run -dt --pod new:frontend -p 3306:3306 -e MARIADB_ROOT_PASSWORD=Password123! mariadb/server:10.4
+
 podman pod start frontend
