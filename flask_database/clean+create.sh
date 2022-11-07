e60aa65885c2bc7f051bb732b79d8e9f98844733
andrewcgaitskell <andrew@gaitskell.com>
2022-11-07 11:21:13 +0000

    Update clean+create.sh

--- flask_database/clean+create.sh
+++ flask_database/clean+create.sh
@@ -5,6 +5,6 @@ cd /home/andrew_gaitskell/project/flask-mariadb-nginx/flask_database
 podman build -t my-flaskdatabase-1 .
 cd /home/andrew_gaitskell/project/flask-mariadb-nginx/nginx
 podman build -t my-nginx-1 .
-podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx-1:latest
+podman run -dt --pod new:frontend -p 80:80 localhost/my-nginx-1:latest
 podman run -dt --pod frontend localhost/my-flaskdatabase-1:latest
 podman pod start frontend
