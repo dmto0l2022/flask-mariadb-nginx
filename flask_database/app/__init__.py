aed65cb285f4b462fa1c711b0e6cf650689189cd
andrewcgaitskell <andrew@gaitskell.com>
2022-11-07 14:07:58 +0000

    Update __init__.py

--- flask_database/app/__init__.py
+++ flask_database/app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 
-from config import Config
+#from config import Config
+
+import os
+
+from os import environ, path
+
+from dotenv import load_dotenv
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['FLASK_SECRET'] = "a"
 
 from app import routes
