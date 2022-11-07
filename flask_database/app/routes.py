b8e5ca0b9cdf77f8c88ddcae5634e1d067343482
andrewcgaitskell <andrew@gaitskell.com>
2022-11-07 11:32:04 +0000

    Update routes.py

--- flask_database/app/routes.py
+++ flask_database/app/routes.py
@@ -4,3 +4,8 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/user')
+def index():
+    user = {'username': 'Andy'}
+    return render_template('basic.html', title='Basic', user=user)
