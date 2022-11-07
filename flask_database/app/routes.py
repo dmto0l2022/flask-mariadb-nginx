4156742de02d772780b04d8c509685fa22ed2670
andrewcgaitskell <andrew@gaitskell.com>
2022-11-07 14:21:06 +0000

    Update routes.py

--- flask_database/app/routes.py
+++ flask_database/app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 def hello():
@@ -14,3 +15,9 @@ def index():
 def user():
     user = {'username': 'Andy'}
     return render_template('user.html', title='Basic', user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
