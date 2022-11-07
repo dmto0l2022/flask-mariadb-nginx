159e58cd13806eb8fa2279d23ee9747e34d20feb
andrewcgaitskell <andrew@gaitskell.com>
2022-11-07 14:32:43 +0000

    Update __init__.py

--- flask_database/app/__init__.py
+++ flask_database/app/__init__.py
@@ -9,9 +9,12 @@ from os import environ, path
 from dotenv import load_dotenv
 
 BASE_DIR = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(BASE_DIR, ".env"))
+load_dotenv(path.join(BASE_DIR, "app/.env"))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = environ.get('FLASK_SECRET_KEY')
+print('key from file: ')
+print(environ.get('FLASK_SECRET_KEY'))
+
+app.config['SECRET_KEY'] = '1234567890' # environ.get('FLASK_SECRET_KEY')
 
 from app import routes
