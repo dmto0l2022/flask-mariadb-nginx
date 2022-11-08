718e50ffa8fae98e1dd211e46bc0ca2d71f91ab4
andrewcgaitskell <andrew@gaitskell.com>
2022-11-08 15:20:14 +0000

    Update create.sh

--- nginx_privileged/create.sh
+++ nginx_privileged/create.sh
@@ -3,4 +3,16 @@ podman pod rm frontend
 podman rmi -a
 cd /opt/dmtools/code/flask-mariadb-nginx/nginx_privileged
 podman build -t my-nginx_priv-1 .
-podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx_priv-1:latest
+podman run -detach --pod new:frontend -p 8080:80 localhost/my-nginx_priv-1:latest
+
+
+#podman run --detach \
+#--pod frontend \
+#--restart=always \
+#-e MARIADB_ROOT_PASSWORD="pythonuser" \
+#-e MARIADB_DATABASE="world" \
+#-e MARIADB_USER="pythonuser" \
+#-e MARIADB_PASSWORD="pythonuser" \
+#-e MARIADB_ROOT_HOST="localhost" \
+#--name=mariadb1 \
+# mariadb
