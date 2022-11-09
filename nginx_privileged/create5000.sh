8d5639ca9eaaecd34b4418a4bcf0add5697925dc
andrewcgaitskell <andrew@gaitskell.com>
2022-11-09 16:14:10 +0000

    Update create5000.sh

--- nginx_privileged/create5000.sh
+++ nginx_privileged/create5000.sh
@@ -1,12 +1,16 @@
 podman pod stop pod-backend
 podman pod rm pod-backend
 
+#podman build -t my-flask5000-1 .
+#podman run -dt --pod new:frontend --network=bridge -p 5000:5000 localhost/my-flask5000-1:latest
+#podman pod start frontend
+
 podman pod create \
 --name pod-backend \
 --infra \
 --infra-name infra-backend \
 --publish 5000:5000 \
---network bridge
+--network=bridge
 
 cd /opt/dmtools/code/flask-mariadb-nginx/flask5000
 
@@ -15,7 +19,6 @@ podman build -t my-flask5000-1 .
 podman run -detach \
 --name flask-1 \
 --pod pod-backend \
---network bridge \
 localhost/my-flask5000-1:latest
 
 cd /opt/dmtools/code/flask-mariadb-nginx/nginx_privileged
