7aa9a469688d8f046ed6dece6d042c8fc575efd9
andrewcgaitskell <andrew@gaitskell.com>
2022-11-09 15:24:48 +0000

    Update clean+create.sh

--- flask5000/clean+create.sh
+++ flask5000/clean+create.sh
@@ -2,5 +2,5 @@ podman pod stop frontend
 podman pod rm frontend
 podman rmi -a
 podman build -t my-flask5000-1 .
-podman run -dt --pod new:frontend --network=host -p 5000 localhost/my-flask5000-1:latest
+podman run -dt --pod new:frontend --network=host -p 5000:5000 localhost/my-flask5000-1:latest
 podman pod start frontend
