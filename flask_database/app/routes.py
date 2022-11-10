54bdec4cd7eaad760970c5aa9c117c2031657d24
andrewcgaitskell <andrew@gaitskell.com>
2022-11-10 17:47:14 +0000

    Update routes.py

--- flask_database/app/routes.py
+++ flask_database/app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+#from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -16,8 +17,17 @@ def user():
     user = {'username': 'Andy'}
     return render_template('user.html', title='Basic', user=user)
 
-@app.route('/login')
+#@app.route('/login')
+#def login():
+#    form = LoginForm()
+#    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
