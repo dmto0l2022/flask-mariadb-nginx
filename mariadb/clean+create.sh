963df27e15117c807c1891766a2342472ee45210
andrewcgaitskell <andrew@gaitskell.com>
2022-11-10 17:18:36 +0000

    Update clean+create.sh

--- mariadb/clean+create.sh
+++ mariadb/clean+create.sh
@@ -1,7 +1,7 @@
 podman pod stop frontend
 podman pod rm frontend
 podman rmi -a
-cd /home/andrew_gaitskell/project/flask-mariadb-nginx/mariadb
+cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
 podman build -t my-mariadb-1 .
 podman run -dt --pod new:frontend -p 3306:3306 localhost/my-mariadb-1:latest
 podman pod start frontend
