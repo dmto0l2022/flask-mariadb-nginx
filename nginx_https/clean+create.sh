533bf79241cd112dbd831f98f2958c176f2282ff
andrewcgaitskell <andrew@gaitskell.com>
2022-11-10 15:23:17 +0000

    Update clean+create.sh

--- nginx_https/clean+create.sh
+++ nginx_https/clean+create.sh
@@ -2,6 +2,6 @@ podman pod stop frontend
 podman pod rm frontend
 cd /home/andrew_gaitskell/project/flask-mariadb-nginx/nginx_https
 podman build -t my-nginx-1 .
-# podman run -dt --pod new:frontend -p 8080:80 -v /etc/letsencrypt:/etc/letsencrypt localhost/my-nginx-1:latest
-podman run -dt --pod new:frontend -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt localhost/my-nginx-1:latest
+podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx-1:latest
+# podman run -dt --pod new:frontend -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt localhost/my-nginx-1:latest
 podman pod start frontend
