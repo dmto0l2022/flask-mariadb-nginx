b912229ea49eea9e128341c8e4ae214251249534
andrewcgaitskell <andrew@gaitskell.com>
2022-11-10 17:29:35 +0000

    Update clean+create.sh

--- flask_database/clean+create.sh
+++ flask_database/clean+create.sh
@@ -1,14 +1,34 @@
-podman pod stop frontend
-podman pod rm frontend
+podman pod stop pod-backend
+podman pod rm pod-backend
 podman rmi -a
+
+
+podman pod create \
+--name pod-backend \
+--infra-name infra-backend \
+--network bridge \
+--publish 8080:80
+
 cd /opt/dmtools/code/flask-mariadb-nginx/flask_database
 podman build -t my-flaskdatabase-1 .
 cd /opt/dmtools/code/flask-mariadb-nginx/nginx
 podman build -t my-nginx-1 .
 cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
 podman build -t my-mariadb-1 .
-podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx-1:latest
-podman run -dt --pod frontend localhost/my-flaskdatabase-1:latest
-podman run -dt --pod frontend localhost/my-mariadb-1:latest
+
+podman run -dt \
+--name nginx_backend-1 \
+--pod pod-backend \
+localhost/my-nginx-1:latest
+
+podman run -dt \
+--name flaskapp_backend-1 \
+--pod pod-backend \
+localhost/my-flaskdatabase-1:latest
+
+podman run -dt \
+--name mariadb_backend-1 \
+--pod pod-backend \
+localhost/my-mariadb-1:latest
 
 cd /opt/dmtools/code/flask-mariadb-nginx/flask_database
