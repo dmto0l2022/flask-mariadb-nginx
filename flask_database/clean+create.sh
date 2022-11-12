2c3a97afbbcf91ea56f7e774be2649e6ee1d6957
andrewcgaitskell <andrew@gaitskell.com>
2022-11-12 15:07:27 +0000

    Update clean+create.sh

--- flask_database/clean+create.sh
+++ flask_database/clean+create.sh
@@ -1,7 +1,5 @@
-podman pod stop pod-backend
-podman pod rm pod-backend
-podman rmi -a
-
+podman pod stop pod-app-backend
+podman pod rm pod-app-backend
 
 podman pod create \
 --name pod-app-backend \
@@ -9,28 +7,29 @@ podman pod create \
 --network bridge \
 --publish 8080:80
 
-podman pod create \
---name pod-data-backend \
---infra-name infra-data-backend \
---network bridge \
---publish 3306:3306
+# podman pod create \
+# --name pod-data-backend \
+# --infra-name infra-data-backend \
+# --network bridge \
+# --publish 3306:3306
 
 cd /opt/dmtools/code/flask-mariadb-nginx/flask_database
 podman build -t my-flaskdatabase-1 .
 cd /opt/dmtools/code/flask-mariadb-nginx/nginx
 podman build -t my-nginx-1 .
-cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
-podman build -t my-mariadb-1 .
+
+# cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
+# podman build -t my-mariadb-1 .
 
 podman run -dt \
 --name nginx_backend-1 \
 --pod pod-app-backend \
 localhost/my-nginx-1:latest
 
-podman run -dt \
---name mariadb_backend-1 \
---pod pod-data-backend \
-localhost/my-mariadb-1:latest
+# podman run -dt \
+# --name mariadb_backend-1 \
+# --pod pod-data-backend \
+# localhost/my-mariadb-1:latest
 
 podman run -dt \
 --name flaskapp_backend-1 \
