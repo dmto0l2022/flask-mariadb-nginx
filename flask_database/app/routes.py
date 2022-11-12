d1874031f99f8ef1840988b46082249fc728bfc1
andrewcgaitskell <andrew@gaitskell.com>
2022-11-12 17:52:59 +0000

    Update routes.py

--- flask_database/app/routes.py
+++ flask_database/app/routes.py
@@ -1,6 +1,7 @@
 #from flask import render_template
 from flask import render_template, flash, redirect, url_for
-from app import app
+##from app import app
+from flask import current_app as app
 from app.forms import LoginForm
 
 @app.route('/')
