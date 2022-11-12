7467ea5613141b7c7857071ffbf7fd75e39b18c6
andrewcgaitskell <andrew@gaitskell.com>
2022-11-12 17:17:50 +0000

    Update __init__.py

--- flask_database/app/__init__.py
+++ flask_database/app/__init__.py
@@ -14,6 +14,7 @@ from dotenv import load_dotenv
 BASE_DIR = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(BASE_DIR, ".env"))
 
+db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
