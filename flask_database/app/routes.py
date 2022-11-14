d425729b2036db4cedbe9f0d77e3651d442661cd
andrewcgaitskell <andrew@gaitskell.com>
2022-11-14 16:28:45 +0000

    Update routes.py

--- flask_database/app/routes.py
+++ flask_database/app/routes.py
@@ -3,6 +3,10 @@ from flask import render_template, flash, redirect, url_for
 ##from app import app
 from flask import current_app as app
 
+login = app.login_manager
+#from app import db
+db = app.extensions['sqlalchemy'].db
+
 from flask_login import current_user, login_user, logout_user
 
 from flask_login import login_required
@@ -27,6 +31,20 @@ def index():
 def user():
     user = {'username': 'Andy'}
     return render_template('user.html', title='Basic', user=user)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 #@app.route('/login')
 #def login():
