3f5d3bf20e1c0f7ef9cbc9920ea5b87e542d2544
andrewcgaitskell <andrew@gaitskell.com>
2022-11-14 10:53:29 +0000

    Update models.py

--- flask_database/app/models.py
+++ flask_database/app/models.py
@@ -2,6 +2,14 @@ from flask import current_app as app
 #from app import db
 db = app.extensions['sqlalchemy'].db
 
+from sqlalchemy import CHAR, Column, DECIMAL, Enum, ForeignKey, text
+from sqlalchemy.dialects.mysql import INTEGER, SMALLINT
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -10,7 +18,50 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Country(Base):
+    __tablename__ = 'country'
+
+    Code = Column(CHAR(3), primary_key=True, server_default=text("''"))
+    Name = Column(CHAR(52), nullable=False, server_default=text("''"))
+    Continent = Column(Enum('Asia', 'Europe', 'North America', 'Africa', 'Oceania', 'Antarctica', 'South America'), nullable=False, server_default=text("'Asia'"))
+    Region = Column(CHAR(26), nullable=False, server_default=text("''"))
+    SurfaceArea = Column(DECIMAL(10, 2), nullable=False, server_default=text("0.00"))
+    IndepYear = Column(SMALLINT(6))
+    Population = Column(INTEGER(11), nullable=False, server_default=text("0"))
+    LifeExpectancy = Column(DECIMAL(3, 1))
+    GNP = Column(DECIMAL(10, 2))
+    GNPOld = Column(DECIMAL(10, 2))
+    LocalName = Column(CHAR(45), nullable=False, server_default=text("''"))
+    GovernmentForm = Column(CHAR(45), nullable=False, server_default=text("''"))
+    HeadOfState = Column(CHAR(60))
+    Capital = Column(INTEGER(11))
+    Code2 = Column(CHAR(2), nullable=False, server_default=text("''"))
+
+class City(Base):
+    __tablename__ = 'city'
+
+    ID = Column(INTEGER(11), primary_key=True)
+    Name = Column(CHAR(35), nullable=False, server_default=text("''"))
+    CountryCode = Column(ForeignKey('country.Code'), nullable=False, index=True, server_default=text("''"))
+    District = Column(CHAR(20), nullable=False, server_default=text("''"))
+    Population = Column(INTEGER(11), nullable=False, server_default=text("0"))
+
+    country = relationship('Country')
+
+
+class Countrylanguage(Base):
+    __tablename__ = 'countrylanguage'
+
+    CountryCode = Column(ForeignKey('country.Code'), primary_key=True, nullable=False, index=True, server_default=text("''"))
+    Language = Column(CHAR(30), primary_key=True, nullable=False, server_default=text("''"))
+    IsOfficial = Column(Enum('T', 'F'), nullable=False, server_default=text("'F'"))
+    Percentage = Column(DECIMAL(4, 1), nullable=False, server_default=text("0.0"))
+
+    country = relationship('Country')
     
+
+'''    
 class city(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(35), index=True, unique=True)
@@ -21,16 +72,7 @@ class city(db.Model):
     def __repr__(self):
         return '<Name {}>'.format(self.Name)
 
-class country(db.Model):
-    Code = db.Column(db.String(3), primary_key=True)
-    Name = db.Column(db.String(35), index=True, unique=True)
-    Continent = db.Column(db.String(20), index=True, unique=False)
-    Region = db.Column(db.String(20), index=True, unique=False)
-    Population = db.Column(db.Integer, index=True, unique=False)
 
-    def __repr__(self):
-        return '<Name {}>'.format(self.Name)    
-'''
 -- world.country definition
 
 CREATE TABLE `country` (
