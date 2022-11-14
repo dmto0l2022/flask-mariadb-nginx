8f8a4bab9b884f94d0e1cf6a38c13d84f80ed68c
andrewcgaitskell <andrew@gaitskell.com>
2022-11-14 11:55:13 +0000

    Update models.py

--- flask_database/app/models.py
+++ flask_database/app/models.py
@@ -3,6 +3,9 @@ from flask import current_app as app
 #from app import db
 db = app.extensions['sqlalchemy'].db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 from sqlalchemy import CHAR, Column, DECIMAL, Enum, ForeignKey, text, DateTime
 from sqlalchemy.dialects.mysql import INTEGER, SMALLINT
 from sqlalchemy.orm import relationship
@@ -17,6 +20,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -25,7 +34,7 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=dt.datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    
     def __repr__(self):
         return '<Post {}>'.format(self.body)    
     
