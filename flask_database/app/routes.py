acccdbed1d6672452e3fa96ddbd7a40d49011679
andrewcgaitskell <andrew@gaitskell.com>
2022-11-14 12:41:24 +0000

    Update routes.py

--- flask_database/app/routes.py
+++ flask_database/app/routes.py
@@ -5,6 +5,8 @@ from flask import current_app as app
 
 from flask_login import current_user, login_user, logout_user
 
+from flask_login import login_required
+
 from app.models import User
 
 from app.forms import LoginForm
@@ -14,6 +16,7 @@ def hello():
     return "Hello, World!"
 
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Andy'}
     return render_template('index.html', title='Index', user=user)
@@ -51,6 +54,21 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+'''
+def login():
+    # ...
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+'''
 
 @app.route('/logout')
 def logout():
