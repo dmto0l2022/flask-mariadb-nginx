5d084bc6f65fc75ccdae28bd7f66130ed9857620
andrewcgaitskell <andrew@gaitskell.com>
2022-11-15 16:58:32 +0000

    Update and rename prefix.ap to prefix.py

--- flask_database/prefix.py
+++ flask_database/prefix.py
@@ -1,12 +1,24 @@
 from flask import Flask, redirect, url_for
 from flask_dance.contrib.github import make_github_blueprint, github
 
+from os import environ, path
+
+from dotenv import load_dotenv
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
+
+app.config['SECRET_KEY'] = environ.get('FLASK_SECRET_KEY')
+GITHUB_OAUTH_CLIENT_ID = environ.get("GITHUB_OAUTH_CLIENT_ID")
+GITHUB_OAUTH_CLIENT_SECRET = environ.get("GITHUB_OAUTH_CLIENT_SECRET")
+
 app = Flask(__name__)
 app.secret_key = "supersekrit"
 blueprint = make_github_blueprint(
-    client_id="my-key-here",
-    client_secret="my-secret-here",
+    client_id=GITHUB_OAUTH_CLIENT_ID,
+    client_secret=GITHUB_OAUTH_CLIENT_SECRET,
 )
+
 app.register_blueprint(blueprint, url_prefix="/login")
 
 @app.route("/")
