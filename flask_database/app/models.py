87587bb579401926030ad288936dba1a026be9ed
andrewcgaitskell <andrew@gaitskell.com>
2022-11-15 09:57:24 +0000

    Update models.py

--- flask_database/app/models.py
+++ flask_database/app/models.py
@@ -34,6 +34,7 @@ class User(UserMixin , db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(256))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -44,6 +45,11 @@ class User(UserMixin , db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+class OAuth(OAuthConsumerMixin, db.Model):
+    provider_user_id = db.Column(db.String(256), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)    
+    
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
