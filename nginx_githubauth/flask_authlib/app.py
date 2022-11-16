6361558d5659faa73d7c45f6ff08ea2fe265c110
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-16 17:21:28 +0000

    Update app.py

--- nginx_githubauth/flask_authlib/app.py
+++ nginx_githubauth/flask_authlib/app.py
@@ -1,16 +1,36 @@
+import os
+
+from os import environ, path
+
+from dotenv import load_dotenv
+
 from flask import Flask, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 
 
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
+
+print(BASE_DIR)
+
+GITHUB_OAUTH_CLIENT_ID = environ.get("GITHUB_OAUTH_CLIENT_ID")
+GITHUB_OAUTH_CLIENT_SECRET = environ.get("GITHUB_OAUTH_CLIENT_SECRET")
+
+print("client:" ,GITHUB_OAUTH_CLIENT_ID)
+print("secret:" ,GITHUB_OAUTH_CLIENT_SECRET)
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"]="1"
+
 app.secret_key = 'myscretkey'
 #configuraciones de oauth
 oauth = OAuth(app)
 github = oauth.register(
     name='github',
-    client_id='',
-    client_secret='',
+    client_id=GITHUB_OAUTH_CLIENT_ID,
+    client_secret=GITHUB_OAUTH_CLIENT_SECRET,
     access_token_url='https://github.com/login/oauth/access_token',
     access_token_params=None,
     authorize_url='https://github.com/login/oauth/authorize',
