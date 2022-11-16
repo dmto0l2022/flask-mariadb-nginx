c2a91a4e7165cb215f33a12cd13501d0202d8a6a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-16 11:00:44 +0000

    Update clean+create.sh

--- nginx_githubauth/nginx/clean+create.sh
+++ nginx_githubauth/nginx/clean+create.sh
@@ -1,9 +1,7 @@
-podman pod stop frontend
-podman pod rm frontend
 cd /opt/dmtools/code/flask-mariadb-nginx/nginx_githubauth/nginx
 podman build -t my-nginx-1 .
-#podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx-1:latest
-podman run -dt --pod new:frontend -p 80:80 localhost/my-nginx-1:latest
+#podman run -dt --pod new:nginx-frontend -p 8080:80 localhost/my-nginx-1:latest
+podman run -dt --pod new:nginx-frontend -network Bridge -p 80:80 localhost/my-nginx-1:latest
 ## you can add 'net.ipv4.ip_unprivileged_port_start=80' to /etc/sysctl.conf (currently 1024)
 #podman run -dt --pod new:frontend -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt localhost/my-nginx-1:latest
 podman pod start frontend
