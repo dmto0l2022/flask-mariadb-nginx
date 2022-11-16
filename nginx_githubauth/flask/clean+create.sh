b8c2c6a22eecdbef6a6a03b589445ce1dc884453
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-16 11:01:24 +0000

    Update clean+create.sh

--- nginx_githubauth/flask/clean+create.sh
+++ nginx_githubauth/flask/clean+create.sh
@@ -1,2 +1,2 @@
 podman build -t my-flask5000-1 .
-podman run -dt --pod new:frontend --network=bridge -p 5000:5000 localhost/my-flask5000-1:latest
+podman run -dt --pod new:flask-frontend --network=bridge -p 5000:5000 localhost/my-flask5000-1:latest
