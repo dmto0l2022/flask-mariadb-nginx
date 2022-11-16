f14028b55a3d4514eae985a434a794f85416e032
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-16 09:35:44 +0000

    Update clean+create.sh

--- nginx_githubauth/nginx/clean+create.sh
+++ nginx_githubauth/nginx/clean+create.sh
@@ -1,6 +1,6 @@
 podman pod stop frontend
 podman pod rm frontend
-cd /opt/dmtools/code/flask-mariadb-nginx/nginx_githubauth
+cd /opt/dmtools/code/flask-mariadb-nginx/nginx_githubauth/nginx
 podman build -t my-nginx-1 .
 podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginx-1:latest
 #podman run -dt --pod new:frontend -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt localhost/my-nginx-1:latest
