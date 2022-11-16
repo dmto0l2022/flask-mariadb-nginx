2850817c30a93a8ddc19f8d7e7b24415924d36f2
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-16 09:39:14 +0000

    Create app.py

---
+++ nginx_githubauth/flask/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.github import make_github_blueprint, github
+
+from os import environ, path
+
+from dotenv import load_dotenv
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
+
+GITHUB_OAUTH_CLIENT_ID = environ.get("GITHUB_OAUTH_CLIENT_ID")
+GITHUB_OAUTH_CLIENT_SECRET = environ.get("GITHUB_OAUTH_CLIENT_SECRET")
+
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+app = Flask(__name__)
+
+app.wsgi_app = PrefixMiddleware(app.wsgi_app)##, prefix='/app')                    
+                    
+                    
+app.config['SECRET_KEY'] = environ.get('FLASK_SECRET_KEY')
+blueprint = make_github_blueprint(
+    client_id=GITHUB_OAUTH_CLIENT_ID,
+    client_secret=GITHUB_OAUTH_CLIENT_SECRET,
+)
+
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route("/")
+def index():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok
+    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
