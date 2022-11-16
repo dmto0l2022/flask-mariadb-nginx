60fe702fa8976d4bbb57956cba0e1c4b356e558e
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-16 09:48:23 +0000

    Update app.py

--- nginx_githubauth/flask/app.py
+++ nginx_githubauth/flask/app.py
@@ -49,4 +49,4 @@ def index():
     return "You are @{login} on GitHub".format(login=resp.json()["login"])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
