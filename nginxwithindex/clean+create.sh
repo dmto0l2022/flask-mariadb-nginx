e871e08e05c8a60ef09ba56d2ec3fa9eb71dc0df
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-17 12:11:37 +0000

    Update clean+create.sh

--- nginxwithindex/clean+create.sh
+++ nginxwithindex/clean+create.sh
@@ -1,10 +1,6 @@
-podman pod stop frontend
-podman pod rm frontend
-podman rmi -a
-cd /home/andrew_gaitskell/project/flask-mariadb-nginx/flask_withpath
-podman build -t my-flask-1 .
-cd /home/andrew_gaitskell/project/flask-mariadb-nginx/nginxwithindex
+podman pod stop frontend-nginx
+podman pod rm frontend-nginx
+cd /opt/dmtools/code/flask-mariadb-nginx/nginxwithindex
 podman build -t my-nginxwithindex-1 .
-podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginxwithindex-1:latest
-podman run -dt --pod frontend localhost/my-flask-1:latest
-podman pod start frontend
+#podman run -dt --pod new:frontend -p 8080:80 localhost/my-nginxwithindex-1:latest
+podman run -dt --pod new:frontend -p 80:80 localhost/my-nginxwithindex-1:latest
