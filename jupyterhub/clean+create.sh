ff0f74509dc8a206faa0d51eea86fc7c872b8ff4
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-11-20 18:26:19 -0500

    Update clean+create.sh

--- jupyterhub/clean+create.sh
+++ jupyterhub/clean+create.sh
@@ -2,5 +2,5 @@ podman pod stop jupyter-frontend
 podman pod rm jupyter-frontend
 cd /opt/dmtools/code/flask-mariadb-nginx/jupyterhub
 podman build -t my-jupyterhub-1 .
-podman run -dt --pod new:jupyter-frontend -p 8000:8000 localhost/my-jupyterhub-1:latest
+podman run -dt --pod new:jupyter-frontend -p 8000:8000 -p 8001:8001 -p 8002:8002 localhost/my-jupyterhub-1:latest
 podman pod start jupyter-frontend
