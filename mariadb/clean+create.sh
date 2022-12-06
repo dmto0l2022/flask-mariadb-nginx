a48ef2c07d94f3b7e1fec4e2c7ee9c4536397e78
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-06 16:40:46 +0000

    Update clean+create.sh

--- mariadb/clean+create.sh
+++ mariadb/clean+create.sh
@@ -1,6 +1,16 @@
-podman pod stop db-frontend
-podman pod rm db-frontend
+podman pod stop pod-db-backend
+podman pod rm pod-db-backend
 cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
+
+podman pod create \
+--name pod-db-backend \
+--infra-name infra-db-backend \
+--network bridge \
+--publish 3306:3306
+
 podman build -t my-mariadb-1 .
-podman run -dt --pod new:db-frontend -p 3306:3306 localhost/my-mariadb-1:latest
-podman pod start db-frontend
+
+podman run -dt \
+--name db_backend-1 \
+--pod pod-db-backend \
+localhost/my-mariadb-1:latest
