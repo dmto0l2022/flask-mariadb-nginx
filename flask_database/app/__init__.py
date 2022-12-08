0d4fc39ebdd036e65a7f0649d441c00b124fb224
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-08 10:21:10 +0000

    Update __init__.py

--- flask_database/app/__init__.py
+++ flask_database/app/__init__.py
@@ -141,19 +141,19 @@ def create_app():
         app.config['SECURITY_REGISTERABLE'] = True
         app.config['SECURITY_REGISTER_URL'] = '/app/register'
         app.config['SECURITY_RESET_URL'] = '/app/reset'
-        SECURITY_CHANGE_URL
-        SECURITY_CONFIRM_URL
-        SECURITY_POST_LOGIN_VIEW
-        SECURITY_POST_LOGOUT_VIEW
-        SECURITY_POST_REGISTER_VIEW
-        SECURITY_POST_CONFIRM_VIEW
-        SECURITY_POST_RESET_VIEW
-        SECURITY_POST_CHANGE_VIEW
-        SECURITY_UNAUTHORIZED_VIEW
-        SECURITY_CONFIRMABLE=True
-        SECURITY_RECOVERABLE=True
-        SECURITY_TRACKABLE=True
-        SECURITY_CHANGEABLE=True
+        app.config['SECURITY_CHANGE_URL'] = '/app/change'
+        app.config['SECURITY_CONFIRM_URL'] = '/app/confirm'
+        app.config['SECURITY_POST_LOGIN_VIEW'] = '/app/welcome'
+        app.config['SECURITY_POST_LOGOUT_VIEW'] = '/app/login'
+        app.config['SECURITY_POST_REGISTER_VIEW'] = '/app/welcome'
+        app.config['SECURITY_POST_CONFIRM_VIEW'] = '/app/welcome'
+        app.config['SECURITY_POST_RESET_VIEW'] = '/app/welcome'
+        app.config['SECURITY_POST_CHANGE_VIEW'] = '/app/welcome'
+        app.config['SECURITY_UNAUTHORIZED_VIEW'] = '/app/register'
+        app.config['SECURITY_CONFIRMABLE'] = True
+        app.config['SECURITY_RECOVERABLE'] = True
+        app.config['SECURITY_TRACKABLE='] = True
+        app.config['SECURITY_CHANGEABLE'] = True
         
         # Setup Flask-Security
         user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
