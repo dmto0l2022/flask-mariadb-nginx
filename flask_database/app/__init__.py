505bdbdcd12c6e810c7181871588adc919923d10
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-08 09:31:42 +0000

    Update __init__.py

--- flask_database/app/__init__.py
+++ flask_database/app/__init__.py
@@ -40,12 +40,19 @@ def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     
+    mail_server = environ.get('MAIL_SERVER')
+    mail_port = environ.get('MAIL_PORT')
+    mail_username = environ.get('MAIL_USERNAME')
+    mail_password = environ.get('MAIL_PASSWORD')
+    sender_email = environ.get('MAIL_SENDEREMAIL')
+    receiver_email = environ.get('MAIL_RECEIVEREMAIL')
+    
     # After 'Create app'
-    app.config['MAIL_SERVER'] = 'smtp.example.com'
-    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_SERVER'] = mail_server
+    app.config['MAIL_PORT'] = mail_port
     app.config['MAIL_USE_SSL'] = True
-    app.config['MAIL_USERNAME'] = 'username'
-    app.config['MAIL_PASSWORD'] = 'password'
+    app.config['MAIL_USERNAME'] = mail_username
+    app.config['MAIL_PASSWORD'] = mail_password
     
     db = SQLAlchemy()
         
