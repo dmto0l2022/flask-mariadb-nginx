9fab35f27731170442f246b9c265f8dfee483667
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-09 09:50:25 +0000

    Update stopall.sh

--- flask_database/stopall.sh
+++ flask_database/stopall.sh
@@ -1,6 +1,6 @@
 podman stop flaskapp_backend-1
-podman stop infra-app-backend
+##podman stop infra-app-backend
 
 podman stop db_backend-1
-podman stop infra-db-backend
+##podman stop infra-db-backend
 
