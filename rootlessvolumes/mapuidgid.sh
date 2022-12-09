8feefd42a29ee8ce8a2ea128e12903e08e4ed4e6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-09 16:36:00 +0000

    Update mapuidgid.sh

--- rootlessvolumes/mapuidgid.sh
+++ rootlessvolumes/mapuidgid.sh
@@ -14,3 +14,25 @@ podman run --rm \
   --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
      docker.io/library/ubuntu /bin/cat /proc/self/uid_map
      
+     
+podman run -dit --volume /src:/dest:z \
+  --user $uid:$gid \
+  --uidmap 0:1:$uid \
+  --uidmap $uid:0:1 \
+  --uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+  --gidmap 0:1:$gid \
+  --gidmap $gid:0:1 \
+  --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+ --name busybox busybox
+ 
+ 
+ podman run -dit --volume /opt/dmtools/mysql:/dest:z \
+  --user $uid:$gid \
+  --uidmap 0:1:$uid \
+  --uidmap $uid:0:1 \
+  --uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+  --gidmap 0:1:$gid \
+  --gidmap $gid:0:1 \
+  --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+ --name busybox1 busybox
+     
