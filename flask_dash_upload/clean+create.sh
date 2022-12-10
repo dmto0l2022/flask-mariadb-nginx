accec33dfc8adae91ad9a1acecafd78df4020141
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-10 10:13:33 +0000

    Create clean+create.sh

---
+++ flask_dash_upload/clean+create.sh
@@ -0,0 +1,34 @@
+podman pod stop pod-upload-backend
+podman pod rm pod-upload-backend
+cd /opt/dmtools/code/flask-mariadb-nginx/flask_dash_upload
+
+uid=1001
+gid=1002
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman pod create \
+--name pod-upload-backend \
+--infra-name infra-upload-backend \
+--network bridge \
+--uidmap 0:1:$uid \
+--uidmap $uid:0:1 \
+--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+--gidmap 0:1:$gid \
+--gidmap $gid:0:1 \
+--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--publish 3306:3306
+
+podman build -t my-dashupload-1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name upload_backend-1 \
+--pod pod-upload-backend \
+--volume /opt/dmtools/mysql:/var/lib/mysql:z \
+--user $uid:$gid \
+localhost/my-dashupload-1:latest
+
