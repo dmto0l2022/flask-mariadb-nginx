121292860232112088e35dda17ca80658f3ddd00
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 10:56:16 +0000

    Create routes.py

---
+++ flask_dash_upload/upload/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+route_blueprint = Blueprint('route_blueprint', __name__)
+
+@route_blueprint.route('/upload/files', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        rec = Photo(filename=filename, user=g.user.id)
+        rec.store()
+        flash("Photo saved.")
+        return redirect(url_for('show', id=rec.id))
+    return render_template('upload.html')
+
+@route_blueprint.route('/upload/photo/<id>')
+def show(id):
+    photo = Photo.load(id)
+    if photo is None:
+        abort(404)
+    url = photos.url(photo.filename)
+    return render_template('show.html', url=url, photo=photo)
+
+@route_blueprint.route('/upload')
+def index():
+     return "Hello"
