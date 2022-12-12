a8907e49b7dbdb2ee412354579b01450845e78a6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 11:40:06 +0000

    Update routes.py

--- flask_dash_upload/upload/routes.py
+++ flask_dash_upload/upload/routes.py
@@ -13,7 +13,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/app/upload/files', methods=['GET', 'POST'])
+@route_blueprint.route('/app/upload/files', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
