5784c3d5583707213885b3d16a7689cd856e3435
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 17:35:05 +0000

    Update entry-point.sh

--- flask_dash_upload/entry-point.sh
+++ flask_dash_upload/entry-point.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-/app/env/bin/python3 flask db init
-/app/env/bin/python3 flask db migrate
-/app/env/bin/python3 flask db upgrade
-/app/env/bin/python3 ./app/wsgi.py
+/workdir/app/env/bin/python3 /workdir/app/wsgi.py
