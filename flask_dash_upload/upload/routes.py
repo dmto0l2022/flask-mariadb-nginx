18a6042ee57e611cedc287744c602f9329cefb16
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 14:43:04 +0000

    Update routes.py

--- flask_dash_upload/upload/routes.py
+++ flask_dash_upload/upload/routes.py
@@ -5,6 +5,10 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
+from datetime import datetime
+
+
+
 UPLOAD_FOLDER = '/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
@@ -24,12 +28,23 @@ def upload_file():
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
+        now = datetime.now()
+        iso_date = now.strftime('%Y%m%d%H%M%S%f')
+        extension = file.filename.rsplit('.', 1)[1].lower()
+        newfolder = iso_date
+        newfilename = "data." + extension
+        path2folder = os.path.join(current_app.config['UPLOAD_FOLDER'], newfolder) 
+        try:
+            os.makedirs(path2folder)    
+        except FileExistsError:
+            print("Directory " , path2folder ,  " already exists")  
+        
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            #filename = secure_filename(file.filename)
+            file.save(path2folder, newfilename)
             ##return redirect(url_for('download_file', name=filename))
     return render_template('upload.html')
 
