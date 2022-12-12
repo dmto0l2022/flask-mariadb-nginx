f94a6ee228d2354216f5901edebc03ee61d166d2
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 18:06:03 +0000

    Update __init__.py

--- flask_dash_upload/app/__init__.py
+++ flask_dash_upload/app/__init__.py
@@ -37,8 +37,8 @@ def create_app():
 
     init_db()
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    from hello.routes import hello_page_bp
-    from upload.routes import upload_page_bp
+    from app.hello.routes import hello_page_bp
+    from app.upload.routes import upload_page_bp
     app.register_blueprint(hello_page_bp)
     app.register_blueprint(upload_page_bp)
     
