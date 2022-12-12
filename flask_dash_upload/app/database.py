a71a3b3732fc8070ef877440c69419e4c7c035c1
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 17:12:07 +0000

    Update database.py

--- flask_dash_upload/app/database.py
+++ flask_dash_upload/app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
 from os import environ, path
 
 from dotenv import load_dotenv
@@ -10,15 +9,15 @@ from dotenv import load_dotenv
 BASE_DIR = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(BASE_DIR, ".env"))
 
-MARIADB_USERNAME =
-MARIADB_PASSWORD =
-MARIADB_CONTAINER =
-MARIADB_DATABASE =
+MARIADB_USERNAME = environ.get('MARIADB_USERNAME')
+MARIADB_PASSWORD = environ.get('MARIADB_PASSWORD')
+MARIADB_CONTAINER = environ.get('MARIADB_CONTAINER')
+MARIADB_DATABASE = environ.get('MARIADB_DATABASE')
 
 SQLALCHEMY_DATABASE_URI =  "mariadb+mariadbconnector://" + MARIADB_USERNAME + ":" + MARIADB_PASSWORD + "@" + MARIADB_CONTAINER + ":3306/" + MARIADB_DATAB$
 
 
-engine = create_engine('sqlite:////tmp/test.db')
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
