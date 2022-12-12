ce4bd3ccc31f64001ee5cc6b5036901d3048be13
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 11:27:03 +0000

    Update app.py

--- flask_dash_upload/app.py
+++ flask_dash_upload/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
 ## both following dead ends
 ##from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 ## from flask_uploads import UploadSet, configure_uploads
 
+import os
+
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/app/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 def create_app():
     app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     from hello.routes import route_blueprint as rbh
     from upload.routes import route_blueprint as rbu
     app.register_blueprint(rbh)
