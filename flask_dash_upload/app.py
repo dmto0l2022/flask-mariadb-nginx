9e5adff9eb6040c970baeabdcf979ce5d4d5d94c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 10:07:16 +0000

    Update app.py

--- flask_dash_upload/app.py
+++ flask_dash_upload/app.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    from hello.routes import route_blueprint
+    app.register_blueprint(route_blueprint)
     return app
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
-
