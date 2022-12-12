2317242bdce3686fd9041f42dc5d76cbc5aa7fb7
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 15:33:46 +0000

    Update app.py

--- flask_dash_upload/app.py
+++ flask_dash_upload/app.py
@@ -11,7 +11,7 @@ from werkzeug.utils import secure_filename
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,  instance_relative_config=True)
     app.config.from_object('myconfig.conf')
     #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     from hello.routes import hello_page_bp
