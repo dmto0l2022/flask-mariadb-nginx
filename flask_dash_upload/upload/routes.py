ad0dd9c51700d56e311771c79c7bde324e19f66d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 11:29:40 +0000

    Update routes.py

--- flask_dash_upload/upload/routes.py
+++ flask_dash_upload/upload/routes.py
@@ -1,6 +1,46 @@
 from flask import Blueprint
+
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/app/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 route_blueprint = Blueprint('route_blueprint', __name__)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload/files', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+'''
 @route_blueprint.route('/upload/files', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
@@ -18,6 +58,7 @@ def show(id):
         abort(404)
     url = photos.url(photo.filename)
     return render_template('show.html', url=url, photo=photo)
+'''
 
 @route_blueprint.route('/upload')
 def index():
