8e9d99f3484c600291931948597b10eae53e77dd
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-12 10:58:40 +0000

    Update app.py

--- flask_dash_upload/app.py
+++ flask_dash_upload/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 
 def create_app():
     app = Flask(__name__)
-    from hello.routes import route_blueprint
-    app.register_blueprint(route_blueprint)
+    from hello.routes import route_blueprint as rbh
+    from upload.routes import route_blueprint as rbu
+    app.register_blueprint(rbh)
+    app.register_blueprint(rbu)
     return app
