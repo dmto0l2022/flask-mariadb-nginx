c032fb217bbcb3907f85c2d8dca85ecb92142e44
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-14 12:37:09 +0000

    Create clean+create.sh

---
+++ jupyterhub2/clean+create.sh
@@ -0,0 +1,29 @@
+uid=1001
+gid=1002
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman pod create \
+--name pod-jupyter-frontend \
+--infra-name infra-jupyter-frontend \
+--network bridge \
+--uidmap 0:1:$uid \
+--uidmap $uid:0:1 \
+--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+--gidmap 0:1:$gid \
+--gidmap $gid:0:1 \
+--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--publish 8000:8000 --publish 8001:8001 --publish 8002:8002
+
+podman build -t my-jupyterhub-1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name jupyter-frontend-1 \
+--pod pod-jupyter-frontend \
+--volume /opt/dmtools/notebooks:/workdir/notebooks:z \
+--user $uid:$gid \
+localhost/my-jupyterhub-1:latest
