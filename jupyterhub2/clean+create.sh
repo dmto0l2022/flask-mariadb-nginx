f0981f71ae11a0562862df60eebc422383348012
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-14 22:20:34 +0000

    Update clean+create.sh

--- jupyterhub2/clean+create.sh
+++ jupyterhub2/clean+create.sh
@@ -5,18 +5,20 @@ subuidSize=$(( $(podman info --format "{{ range \
 subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
-podman network create jupyter_network
+#--uidmap 0:1:$uid \
+#--uidmap $uid:0:1 \
+#--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+#--gidmap 0:1:$gid \
+#--gidmap $gid:0:1 \
+#--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+
+podman network create bridge
+
 
 podman pod create \
 --name pod-jupyterhub \
 --infra-name infra-jupyter-frontend \
---network jupyter_network \
---uidmap 0:1:$uid \
---uidmap $uid:0:1 \
---uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
---gidmap 0:1:$gid \
---gidmap $gid:0:1 \
---gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--network bridge \
 --publish 8000:8000 --publish 8001:8001 --publish 8002:8002
 
 podman build -t my-jupyterhub-1 .
