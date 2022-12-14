ad7606e264beea62beb0c7e204b88e9525d48690
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-14 19:33:56 +0000

    Update clean+create.sh

--- jupyterhub/clean+create.sh
+++ jupyterhub/clean+create.sh
@@ -27,6 +27,7 @@ podman build -t my-jupyterhub-1 .
 podman run -dt \
 --name jupyterhub \
 --pod pod-jupyterhub \
+--volume /var/run/docker.sock:/var/run/docker.sock \
 localhost/my-jupyterhub-1:latest
 
 ##--volume /opt/dmtools/notebooks:/workdir/notebooks:z \
