abbe35c11c4f4f32ba182b7b2d5081d124579559
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-15 11:18:01 +0000

    Update create.sh

--- redhatsocket/create.sh
+++ redhatsocket/create.sh
@@ -1,2 +1,5 @@
 podman build -t my-apache2 .
-podman run -dt --pod new:frontend-apache -p 8080:80 localhost/my-apache2:latest
+podman run -dt --pod new:frontend-apache --name httpd -p 127.0.0.1:8080:80 localhost/my-apache2:latest
+
+##podman pull registry.redhat.io/rhel8/httpd-24$
+##podman create --name httpd -p 127.0.0.1:8080:8080 \    -v ~/www-data:/var/www/html/:Z registry.redhat.io/rhel8/httpd-24
