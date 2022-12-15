bb312f008ae1cf537fa407ae1961ca24a8d1f1b0
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-15 11:05:42 +0000

    Update create.sh

--- redhatsocket/create.sh
+++ redhatsocket/create.sh
@@ -1,6 +1,2 @@
-
-podman pull registry.redhat.io/rhel8/httpd-24
-
-podman create --name httpd -p 0.0.0.0:8000:8000 \
--v /opt/dmtools/code/flask-mariadb-nginx/redhatsocket/www-data:/var/www/html/:Z \
-registry.redhat.io/rhel8/httpd-24
+docker build -t my-apache2 .
+docker run -dit --name my-running-app -p  0.0.0.0:8000:8000 my-apache2
