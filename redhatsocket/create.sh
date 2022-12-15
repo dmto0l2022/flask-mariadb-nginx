9232e19ff2619b17a848dc5c26a384ead4549611
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-15 10:58:34 +0000

    Update create.sh

--- redhatsocket/create.sh
+++ redhatsocket/create.sh
@@ -1,1 +1,6 @@
 
+podman pull registry.redhat.io/rhel8/httpd-24
+
+podman create --name httpd -p 0.0.0.0:8000:8000 \
+-v /opt/dmtools/code/flask-mariadb-nginx/redhatsocket/www-data:/var/www/html/:Z \
+registry.redhat.io/rhel8/httpd-24
