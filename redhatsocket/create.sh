0f2301ecdb437415658a3d986ff7d81b199bbbb8
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-15 11:09:04 +0000

    Update create.sh

--- redhatsocket/create.sh
+++ redhatsocket/create.sh
@@ -1,2 +1,2 @@
-docker build -t my-apache2 .
-docker run -dit --name my-running-app -p  0.0.0.0:8000:8000 my-apache2
+podman build -t my-apache2 .
+podman run -dt --pod new:frontend-apache -p 5000:5000 localhost/my-apache2:latest
