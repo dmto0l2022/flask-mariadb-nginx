acfb8a5685f048211623c3d85687dfad2322ad8e
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-17 19:20:33 +0000

    Create clean+create.sh

---
+++ jupyterhub3/clean+create.sh
@@ -0,0 +1,29 @@
+uid=1001
+gid=1002
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman pod create \
+--name pod-jupyterhub \
+--infra-name infra-jupyterhub \
+--network bridge \
+--uidmap 0:1:$uid \
+--uidmap $uid:0:1 \
+--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+--gidmap 0:1:$gid \
+--gidmap $gid:0:1 \
+--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--publish 8000:8000 --publish 8001:8001 --publish 8002:8002
+
+podman build -t my-jupyterhub-1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name jupyterhub \
+--pod pod-jupyterhub \
+--volume /opt/dmtools/data:/home/agaitske:z \
+--user $uid:$gid \
+localhost/my-jupyterhub-1:latest
