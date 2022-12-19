52b1883159ad0498533555f002d2ee52d997e0bd
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-19 11:10:53 +0000

    Update routes.py

--- flask_dash_upload/app/dash_public/routes.py
+++ flask_dash_upload/app/dash_public/routes.py
@@ -1,7 +1,52 @@
-from flask import Blueprint
+from dash import Dash
+from dash import html
+from werkzeug.wsgi import DispatcherMiddleware
+#from flask import Blueprint
+from flask import current_app, Blueprint, render_template
 dash_public_page_bp = Blueprint('dash_public_page_bp', __name__)
+
+#server = flask.Flask(__name__)
+dash_app1 = Dash(__name__, server = dash_public_page_bp, url_base_pathname='/')
+dash_app1.layout = html.Div([html.H1('Hi there, I am Dash1')])
 
 @dash_public_page_bp.route('/dmtpublic')
 def publiclanding():
-     return "This will be a public dash app"
-  
+     #return "This will be a public dash app"
+     return flask.redirect('/dash1')
+
+
+current_app = DispatcherMiddleware(server, {
+    '/dash1': dash_app1.server
+})
+
+'''
+from dash import Dash
+from werkzeug.wsgi import DispatcherMiddleware
+import flask
+from werkzeug.serving import run_simple
+import dash_html_components as html
+
+server = flask.Flask(__name__)
+dash_app1 = Dash(__name__, server = server, url_base_pathname='/dashboard/')
+dash_app2 = Dash(__name__, server = server, url_base_pathname='/reports/')
+dash_app1.layout = html.Div([html.H1('Hi there, I am Dash1')])
+dash_app2.layout = html.Div([html.H1('Hi there, I am Dash2')])
+@server.route('/')
+@server.route('/hello')
+def hello():
+    return 'hello world!'
+
+@server.route('/dashboard/')
+def render_dashboard():
+    return flask.redirect('/dash1')
+
+
+@server.route('/reports/')
+def render_reports():
+    return flask.redirect('/dash2')
+
+app = DispatcherMiddleware(server, {
+    '/dash1': dash_app1.server,
+    '/dash2': dash_app2.server
+})
+'''
