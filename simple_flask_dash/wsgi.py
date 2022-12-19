40aadfc7fa3a70ecf0f4179366ef5aeb0459e720
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-19 12:18:14 +0000

    Create wsgi.py

---
+++ simple_flask_dash/wsgi.py
@@ -0,0 +1,9 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from app1 import app as app1
+from app2 import app as app2
+
+application = DispatcherMiddleware(flask_app, {
+    '/app1': app1.server,
+    '/app2': app2.server,
+})  
