08cd511a78872ad61ae70cd60f7273a5b4cacf4f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-19 10:58:42 +0000

    Update routes.py

--- flask_dash_upload/app/dash_public/routes.py
+++ flask_dash_upload/app/dash_public/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 dash_public_page_bp = Blueprint('dash_public_page_bp', __name__)
 
-@hello_page_bp.route('/dmtpublic')
+@dash_public_page_bp.route('/dmtpublic')
 def publiclanding():
      return "This will be a public dash app"
   
