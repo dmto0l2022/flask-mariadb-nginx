078c512e7804118114f16f73c862934a8d9693c3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-19 17:34:39 +0000

    Update wsgi.py

--- flask_dash_base/wsgi.py
+++ flask_dash_base/wsgi.py
@@ -3,9 +3,11 @@ from app import create_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from app.dashapps.app1 import app as app1
+from app.dashapps.app2 import app as app2
 
 app = create_app()
 
 application = DispatcherMiddleware(app, {
     '/app1': app1.server,
+    '/app2': app2.server,
 })  
