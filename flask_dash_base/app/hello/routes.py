b754c251a0e76e522961c3af018b53f6f1665b89
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-19 15:59:06 +0000

    Update routes.py

--- flask_dash_base/app/hello/routes.py
+++ flask_dash_base/app/hello/routes.py
@@ -3,4 +3,12 @@ hello_page_bp = Blueprint('hello_page_bp', __name__)
 
 @hello_page_bp.route('/')
 def index():
-     return "Hello"
+     return "Hello this is the landing site"
+
+@app.route('/app/')
+def hello():
+    return "Hello, World!"
+
+@app.route('/app/welcome/')
+def welcome():
+    return render_template('welcome.html')
