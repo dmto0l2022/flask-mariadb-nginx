40bc11cdc6b3c6907954e86d426f31c04f10924a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-19 12:07:31 +0000

    Update wsgi.py

--- flask_dash_upload/wsgi.py
+++ flask_dash_upload/wsgi.py
@@ -1,6 +1,16 @@
 from app import create_app
 
+from werkzeug.wsgi import DispatcherMiddleware
+
+from app.dashapps import app as app1
+
 app = create_app()
+
+application = DispatcherMiddleware(app, {
+    '/app1': app1.server,
+    '/app2': app2.server,
+})  
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='8002')
