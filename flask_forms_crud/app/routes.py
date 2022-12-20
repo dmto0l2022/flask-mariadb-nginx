f658d6e9ce3635752976ca57762d8fb35bc8c2c8
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-20 14:18:08 +0000

    Update routes.py

--- flask_forms_crud/app/routes.py
+++ flask_forms_crud/app/routes.py
@@ -12,6 +12,8 @@ from flask_login import current_user
 
 from flask_security.models import fsqla_v3 as fsqla
 
+from . import notes as notes
+
 # Views
 @current_app.route("/")
 def home():
@@ -26,3 +28,18 @@ def hello():
 @auth_required()
 def welcome():
     return render_template('welcome.html')
+
+@current_app.route("/notes/", methods=["POST", "GET"])
+def view_index():
+    if request.method == "POST":
+        notes.create_note(request.form['text'])
+    return render_template("index_form.html", notes=notes.read_notes())
+
+
+@current_app.route("/notes/edit/<note_id>", methods=["POST", "GET"])
+def edit_note(note_id):
+    if request.method == "POST":
+        notes.update_note(notes.note_id, text=request.form['text'], done=request.form['done'])
+    elif request.method == "GET":
+        notes.delete_note(note_id)
+    return redirect("/notes/", code=302)
