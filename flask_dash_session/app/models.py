42b5188f7880e3b07f1766fcb7a60ad049c51841
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-20 12:18:02 +0000

    Update models.py

--- flask_dash_session/app/models.py
+++ flask_dash_session/app/models.py
@@ -4,22 +4,10 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Boolean, DateTime, Column, Integer, \
                     String, ForeignKey, UnicodeText
 
-class RolesUsers(dbf.Base):
-    __tablename__ = 'roles_users'
-    id = Column(Integer(), primary_key=True)
-    user_id = Column('user_id', Integer(), ForeignKey('user.id'))
-    role_id = Column('role_id', Integer(), ForeignKey('role.id'))
-
-class Role(dbf.Base, RoleMixin):
-    __tablename__ = 'role'
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(80), unique=True)
-    description = Column(String(255))
-    permissions = Column(UnicodeText)
-
 class User(dbf.Base, UserMixin):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True)
     email = Column(String(255), unique=True)
     username = Column(String(255), unique=True, nullable=True)
     password = Column(String(255), nullable=False)
@@ -31,6 +19,4 @@ class User(dbf.Base, UserMixin):
     active = Column(Boolean())
     fs_uniquifier = Column(String(255), unique=True, nullable=False)
     confirmed_at = Column(DateTime())
-    roles = relationship('Role', secondary='roles_users',
-                         backref=backref('users', lazy='dynamic'))
     
