6755c4c592bcc143bdc0825fdc18824c1ca1bcd5
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-20 08:39:17 +0000

    Update security.py

--- flask_security/app/security.py
+++ flask_security/app/security.py
@@ -1,23 +1,24 @@
-##https://stackoverflow.com/questions/14793098/how-to-use-flask-security-register-view
-app.config['SECURITY_REGISTERABLE'] = True
-app.config['SECURITY_REGISTER_URL'] = '/app/register'
-app.config['SECURITY_LOGIN_URL'] = '/app/login'
-app.config['SECURITY_LOGOUT_URL'] = '/app/logout'
-app.config['SECURITY_RESET_URL'] = '/app/reset'
-app.config['SECURITY_CHANGE_URL'] = '/app/change'
-app.config['SECURITY_CONFIRM_URL'] = '/app/confirm'
-app.config['SECURITY_POST_LOGIN_VIEW'] = '/app/welcome'
-app.config['SECURITY_POST_LOGOUT_VIEW'] = '/app/login'
-app.config['SECURITY_POST_REGISTER_VIEW'] = '/app/welcome'
-app.config['SECURITY_POST_CONFIRM_VIEW'] = '/app/welcome'
-app.config['SECURITY_POST_RESET_VIEW'] = '/app/welcome'
-app.config['SECURITY_POST_CHANGE_VIEW'] = '/app/welcome'
-app.config['SECURITY_UNAUTHORIZED_VIEW'] = '/app/register'
-app.config['SECURITY_CONFIRMABLE'] = True
-app.config['SECURITY_RECOVERABLE'] = True
-app.config['SECURITY_TRACKABLE='] = True
-app.config['SECURITY_CHANGEABLE'] = True
+from flask import current_app
+from flask_security import Security, current_user, auth_required, hash_password, \
+     SQLAlchemySessionUserDatastore
 
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
-app.security = Security(app, user_datastore)
+##https://stackoverflow.com/questions/14793098/how-to-use-flask-security-register-view
+current_app.config['SECURITY_REGISTERABLE'] = True
+current_app.config['SECURITY_REGISTER_URL'] = '/app/register'
+current_app.config['SECURITY_LOGIN_URL'] = '/app/login'
+current_app.config['SECURITY_LOGOUT_URL'] = '/app/logout'
+current_app.config['SECURITY_RESET_URL'] = '/app/reset'
+current_app.config['SECURITY_CHANGE_URL'] = '/app/change'
+current_app.config['SECURITY_CONFIRM_URL'] = '/app/confirm'
+current_app.config['SECURITY_POST_LOGIN_VIEW'] = '/app/welcome'
+current_app.config['SECURITY_POST_LOGOUT_VIEW'] = '/app/login'
+current_app.config['SECURITY_POST_REGISTER_VIEW'] = '/app/welcome'
+current_app.config['SECURITY_POST_CONFIRM_VIEW'] = '/app/welcome'
+current_app.config['SECURITY_POST_RESET_VIEW'] = '/app/welcome'
+current_app.config['SECURITY_POST_CHANGE_VIEW'] = '/app/welcome'
+current_app.config['SECURITY_UNAUTHORIZED_VIEW'] = '/app/register'
+current_app.config['SECURITY_CONFIRMABLE'] = True
+current_app.config['SECURITY_RECOVERABLE'] = True
+current_app.config['SECURITY_TRACKABLE='] = True
+current_app.config['SECURITY_CHANGEABLE'] = True
+
