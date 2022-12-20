f8288f503cfd7006e3ffb1851f984e6c40a66aad
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-20 19:08:02 +0000

    Update and rename users.py to users_bp.py

--- flask_crud_starter/app/blueprints/users_bp.py
+++ flask_crud_starter/app/blueprints/users_bp.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 from app.models import User
 
-user_bp = Blueprint('users', __name__)
+user_bp = Blueprint('user_bp', __name__)
 
 @user_bp.route('/')
 def index():
