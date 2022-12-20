35066004c90ce4b2bebb1c1933bc7001e73d5140
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-20 14:54:20 +0000

    Update notes.py

--- flask_forms_crud/app/notes.py
+++ flask_forms_crud/app/notes.py
@@ -3,23 +3,23 @@ from . import models as mdf
 
 def create_note(text):
     note = mdf.Note(text=text)
-    dbf.Base.session.add(note)
-    dbf.Base.session.commit()
-    dbf.Base.session.refresh(note)
+    dbf.db_session.add(note)
+    dbf.db_session.commit()
+    dbf.db_session.refresh(note)
 
 
 def read_notes():
-    return dbf.Base.session.query(mdf.Note).all()
+    return dbf.db_session.query(mdf.Note).all()
 
 
 def update_note(note_id, text, done):
-    dbf.Base.session.query(mdf.Note).filter_by(id=note_id).update({
+    dbf.db_session.query(mdf.Note).filter_by(id=note_id).update({
         "text": text,
         "done": True if done == "on" else False
     })
-    db.session.commit()
+    dbf.db_session.commit()
 
 
 def delete_note(note_id):
-    dbf.Base.session.query(mdf.Note).filter_by(id=note_id).delete()
-    dbf.Base.session.commit()
+    dbf.db_session.query(mdf.Note).filter_by(id=note_id).delete()
+    dbf.db_session.commit()
