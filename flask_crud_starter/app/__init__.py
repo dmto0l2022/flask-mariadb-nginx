9ddf25be1ff0b0be574d56110e8c52aa8b4cbb5f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-20 18:17:05 +0000

    Update __init__.py

--- flask_crud_starter/app/__init__.py
+++ flask_crud_starter/app/__init__.py
@@ -5,15 +5,18 @@ from flask_sqlalchemy import SQLAlchemy
 # Database setup
 db = SQLAlchemy()
 
+from app.database import AppDb
+
+app_db = AppDb()
 
 def init_app():
     """Create Flask application."""
     flask_app = Flask(__name__, instance_relative_config=False)
-
-    db.init_app(flask_app)  # initialise the database for the app
+    app_db.init_app(flask_app)
+    
+    #db.init_app(flask_app)  # initialise the database for the app
 
     with flask_app.app_context():
-        from app import database
         from app.models import User  # this import allows us to create the table if it does not exist
         from app import create_users as cu
         
