e2ec6bd5f1957b40cc782f53a18ad43ca73cdaab
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-20 19:25:58 +0000

    Update users_bp.py

--- flask_crud_starter/app/blueprints/users_bp.py
+++ flask_crud_starter/app/blueprints/users_bp.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 from app.models import User
 
-user_bp = Blueprint('user_bp', __name__)
+users_bp = Blueprint('users_bp', __name__)
 
-@user_bp.route('/')
+@users_bp.route('/')
 def index():
     users = User.get_users()
     return render_template('users_simple.html', users=users)
