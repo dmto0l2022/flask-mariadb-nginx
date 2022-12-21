0fd77f131fc2dffc9a00102d5d97ab47ff3aa4fa
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-21 09:46:00 +0000

    Update 10_create_container.sh

--- jupyterhub4/10_create_container.sh
+++ jupyterhub4/10_create_container.sh
@@ -1,5 +1,5 @@
-podman pull ubuntu:latest
-podman create ubuntu:latest --name localubuntu
+podman pull ubuntu:latest --name localubuntu
+podman create localhost:/localubuntu
 podman cp requirements.txt localubuntu:/requirements.txt
 podman cp 1_create_with_sudo.sh localubuntu:/1_create_with_sudo.sh
 podman cp 2_create_with_user.sh localubuntu:/2_create_with_user.sh
