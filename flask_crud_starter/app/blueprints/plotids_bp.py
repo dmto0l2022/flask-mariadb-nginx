0449364c7b2166949a9cbe8cdf0ded6f323b7972
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-22 10:18:09 +0000

    Update plotids_bp.py

--- flask_crud_starter/app/blueprints/plotids_bp.py
+++ flask_crud_starter/app/blueprints/plotids_bp.py
@@ -1,9 +1,44 @@
 from flask import Blueprint, render_template
 from . import models
-
+from . import db
+from datetime import datetime
 users_bp = Blueprint('plotids_bp', __name__)
 
-@plotid_bp.route('/plotsids/')
+@plotid_bp.route('/plots/new/')
 def index():
-    plotids = Plots.get_plotids()
-    return render_template('plotids_simple.html', plotid=plotids)
+    name = 'new plot'
+    plotid = 'P123455662357989092370'
+    plotids = Plots.create(plotid,name)
+    return jsonify(
+        name=name,
+        plotid=plotid
+    ) 
+
+@plotid_bp.route('/plots/show/')
+def index():
+    allplots = Plots.get_plots(plotid,name)
+    return allplots
+
+ @staticmethod
+    def create(plotid,name):
+        """
+        Create new plot
+        """
+        new_plots = Plots(plotid,name)
+        Base.session.add(new_plots)
+        Base.session.commit()
+
+    @staticmethod
+    def get_plots():
+        """
+        :return: list of user details
+        """
+        plots = [
+            {
+                'id': i.id,
+                'plotid': i.plotid,
+                'name': i.name,
+            }
+            for i in Plots.query.order_by('id').all()
+        ]
+        return plots
