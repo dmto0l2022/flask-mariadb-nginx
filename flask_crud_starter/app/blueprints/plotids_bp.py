473673a370bcc3adfb7a1d7dde41d2b80041b617
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-22 10:57:36 +0000

    Update plotids_bp.py

--- flask_crud_starter/app/blueprints/plotids_bp.py
+++ flask_crud_starter/app/blueprints/plotids_bp.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template
 ##from flask import current_app
 from app.models import Plots
 from datetime import datetime
-import jsonify
 
 plotids_bp = Blueprint('plotids_bp', __name__)
 
@@ -11,13 +10,9 @@ def newplots():
     name = 'new plot'
     plotid = 'P123455662357989092370'
     plotids = Plots.create(plotid,name)
-    return jsonify(
-        name=name,
-        plotid=plotid
-    ) 
+    return json.dumps(name=name,plotid=plotid) 
 
 @plotids_bp.route('/plots/show/')
 def showplots():
-    allplots = Plots.get_plots(plotid,name)
-    return allplots
-
+    allplots = Plots.get_plots()
+    return json.dumps(allplots.serialize())
