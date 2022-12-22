b302464b35d17ebbc4cb5a5227d38208c3ab736b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-22 14:58:57 +0000

    Update forms.py

--- flask_crud_starter/app/forms.py
+++ flask_crud_starter/app/forms.py
@@ -3,6 +3,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+class FormCreatePlot(FlaskForm):
+    """New Plot form."""
+    plotid = StringField(
+        'plotid',
+        [DataRequired()]
+    )
+    name = StringField(
+        'name',
+        [DataRequired()]
+    )
+    submit = SubmitField('Submit')
+
 class EnterNewPlotForm(FlaskForm):
     """New Plot form."""
     plotid = StringField(
