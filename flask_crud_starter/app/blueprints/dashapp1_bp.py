6579a9b336883a2debed35de7b80e586a8ab46f2
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-23 07:25:18 +0000

    Update dashapp1_bp.py

--- flask_crud_starter/app/blueprints/dashapp1_bp.py
+++ flask_crud_starter/app/blueprints/dashapp1_bp.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect
 
-home_bp = Blueprint('dashapp1_bp', __name__)
+dashapp1_bp = Blueprint('dashapp1_bp', __name__)
 
 @dashapp1_bp.route('/dashapp1')
 def dashapp1():
-    return redirect("/wsgi_app1", code=302)
+    return redirect('/wsgi_app1', code=302)
