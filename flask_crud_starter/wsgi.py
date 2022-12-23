ca57674640a07ec5b7cb35b8367139d8675ca2f4
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-23 07:04:00 +0000

    Update wsgi.py

--- flask_crud_starter/wsgi.py
+++ flask_crud_starter/wsgi.py
@@ -1,3 +1,11 @@
 from app import init_app
 
-application = init_app()
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from app.dashapps.interactive_table import app as app1
+
+app = init_app()
+
+application = DispatcherMiddleware(app, {
+    '/app1': app1.server,
+})  
