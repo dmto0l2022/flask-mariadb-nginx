18dea69dcbf93ce437c2be1a0af7b3d456031c4a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-23 09:22:06 +0000

    Update todo_bp.py

--- flask_crud_starter/app/blueprints/todo_bp.py
+++ flask_crud_starter/app/blueprints/todo_bp.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
+from flask_restful import Api, Resource, url_for
 
 todo_bp = Blueprint('todo_bp', __name__)
+api = Api(todo_bp)
 
 @todo_bp.route('/todo', methods=['GET', 'POST'])
 def todoroot():
@@ -9,3 +11,9 @@ def todoroot():
 @todo_bp.route('/todo/home')
 def todohome():
     return 'todo home'
+
+class TodoItem(Resource):
+    def get(self, id):
+        return {'task': 'Say "Hello, World!"'}
+
+api.add_resource(TodoItem, '/todo/list/<int:id>')
