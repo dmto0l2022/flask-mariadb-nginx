1654d1bf1f17cae061802901bffc20bc8fb46ead
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-23 09:28:37 +0000

    Update basic_table.py

--- flask_crud_starter/app/dashapps/basic_table.py
+++ flask_crud_starter/app/dashapps/basic_table.py
@@ -9,7 +9,7 @@ app = Dash(__name__, requests_pathname_prefix='/wsgi_app2/')
 
 ##data_request = requests.get('/plots/getall')
 ##url = "http://localhost:8002/plots/getall/"
-url = "/todo/list/1"
+url = "http://localhost:8002/todo/list/1"
 data_request = requests.get(url)
 print(data_request)
 ##data_frame = pd.read_json(data_request)
