aa2083fe00acf6d4084db9907d19cf98e72f0121
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-23 10:58:40 +0000

    Update wsgi.py

--- flask_crud_api/wsgi.py
+++ flask_crud_api/wsgi.py
@@ -1,8 +1,3 @@
 from app import init_app
 
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
-from app.dashapps.interactive_table import app as app1
-from app.dashapps.basic_table import app as app2
-
 application = init_app()
