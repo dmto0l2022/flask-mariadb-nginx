80d4de7bed2778f5b4eb04e4dd364537a6bd8553
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-23 10:43:30 +0000

    Create clean+create.sh

---
+++ flask_crud_api/clean+create.sh
@@ -0,0 +1,32 @@
+podman pod stop pod-api-backend
+podman pod rm pod-api-backend
+cd /opt/dmtools/code/flask-mariadb-nginx/flask_crud_api
+
+uid=1001
+gid=1002
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman pod create \
+--name pod-api-backend \
+--infra-name infra-api-backend \
+--network bridge \
+--uidmap 0:1:$uid \
+--uidmap $uid:0:1 \
+--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+--gidmap 0:1:$gid \
+--gidmap $gid:0:1 \
+--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--publish 8004:8004
+
+podman build -t my-dbapi-1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name api_backend-1 \
+--pod pod-api-backend \
+--user $uid:$gid \
+localhost/my-dbapi-1:latest
