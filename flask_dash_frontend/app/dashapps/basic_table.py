ce921dd3ae972252eae2e7510ea089ccd814ea2b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2022-12-23 18:42:08 +0000

    Update basic_table.py

--- flask_dash_frontend/app/dashapps/basic_table.py
+++ flask_dash_frontend/app/dashapps/basic_table.py
@@ -48,11 +48,11 @@ class MakeApiCall():
 app = Dash(__name__, requests_pathname_prefix='/wsgi_app2/')
 
 ##data_request = requests.get('/plots/getall')
-##url = "http://localhost:8002/plots/getall/"
+url = "http://10.154.0.20:8004/plots/getall/"
 #url = "http://localhost:8002/todo/list/1"
 #data_request = requests.get(url="http://0.0.0.0:8002/todo/list/1")
 ##url = "http://dev4.dmtools.info:8002/todo/list/1"
-url = "http://10.154.0.20:8004/todo/list/1"
+#url = "http://10.154.0.20:8004/todo/list/1"
 ##10.154.0.20 
 data_request = requests.get(url=url)
 text = json.dumps(data_request, sort_keys=True, indent=4)
