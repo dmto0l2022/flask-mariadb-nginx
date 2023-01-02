fdd4b66bb2a5974d1f9d85782e6593f3dfaf9577
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-02 08:41:16 +0000

    Update recreate.sh

--- aa_build/recreate.sh
+++ aa_build/recreate.sh
@@ -1,1 +1,10 @@
-x
+podman system reset
+
+cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
+
+source clean+create.sh
+
+cd /opt/dmtools/code/flask-mariadb-nginx/flask_crud_api
+
+source clean+create.sh
+
