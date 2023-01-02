c808f14ffa432014519cb543edee9cedc9bdd66d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-02 09:00:26 +0000

    Update clean+create.sh

--- mariadb/clean+create.sh
+++ mariadb/clean+create.sh
@@ -1,5 +1,5 @@
-podman pod stop pod-db-backend
-podman pod rm pod-db-backend
+podman pod stop pod_mariadb_backend
+podman pod rm pod_mariadb_backend
 cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
 
 uid=1001
@@ -10,8 +10,8 @@ subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
 podman pod create \
---name pod-db-backend \
---infra-name infra-db-backend \
+--name pod_mariadb_backend \
+--infra-name infra_mariadb_backend \
 --network bridge \
 --uidmap 0:1:$uid \
 --uidmap $uid:0:1 \
@@ -21,13 +21,13 @@ podman pod create \
 --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
 --publish 3306:3306
 
-podman build -t my-mariadb-1 .
+podman build -t my_mariadb_1 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
---name db_backend-1 \
---pod pod-db-backend \
+--name mariadb_backend_1 \
+--pod pod_mariadb_backend \
 --volume /opt/dmtools/mysql:/var/lib/mysql:z \
 --user $uid:$gid \
-localhost/my-mariadb-1:latest
+localhost/my_mariadb_1:latest
