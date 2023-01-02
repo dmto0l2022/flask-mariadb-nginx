8ef88e022bdfb41df30e01fe6b92df8c08dd3973
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-02 13:49:18 +0000

    Update wsgi.py

--- flask_dash_frontend/wsgi.py
+++ flask_dash_frontend/wsgi.py
@@ -4,10 +4,12 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from app.dashapps.interactive_table import app as app1
 from app.dashapps.basic_table import app as app2
+from app.dashapps.session_app import app as app3
 
 app = init_app()
 
 application = DispatcherMiddleware(app, {
     '/wsgi_app1': app1.server,
     '/wsgi_app2': app2.server,
+    '/session_app': app3.server,
 })  
