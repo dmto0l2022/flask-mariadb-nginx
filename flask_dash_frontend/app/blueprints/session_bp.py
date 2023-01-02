d9b0d483ded666fcc6142eadeba7c5ac5c921649
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-02 12:51:53 +0000

    Update session_bp.py

--- flask_dash_frontend/app/blueprints/session_bp.py
+++ flask_dash_frontend/app/blueprints/session_bp.py
@@ -1,19 +1,9 @@
+## https://testdriven.io/blog/flask-sessions/
 from flask import Blueprint, render_template
 from flask_restful import Api, Resource, url_for
 
-todo_bp = Blueprint('todo_bp', __name__)
-api = Api(todo_bp)
+session_bp = Blueprint('session_bp', __name__)
 
-@todo_bp.route('/todo', methods=['GET', 'POST'])
-def todoroot():
-    return 'todo root'
-
-@todo_bp.route('/todo/home')
-def todohome():
-    return 'todo home'
-
-class TodoItem(Resource):
-    def get(self, id):
-        return {'task': 'Say "Hello, World!"'}
-
-api.add_resource(TodoItem, '/todo/list/<int:id>')
+@session_bp.route('/session', methods=['GET', 'POST'])
+def sessionroot():
+    return 'session root'
