1bb08b0bfa972c59e186d87025bef06274ab04d1
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-02 14:12:17 +0000

    Update session_app.py

--- flask_dash_frontend/app/dashapps/session_app.py
+++ flask_dash_frontend/app/dashapps/session_app.py
@@ -1,12 +1,17 @@
 import dash
+from dash import html
 import dash_bootstrap_components as dbc
 import flask
+from flask import session
 
 server = flask.Flask(__name__)
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],server=server, requests_pathname_prefix='/session_app/')
 
+session_div =html.div(id='session id div', children="S123455667")
+
 app.layout = dbc.Container(
     dbc.Alert("Hello Bootstrap!", color="success"),
+    session_div,
     className="p-5",
 )
