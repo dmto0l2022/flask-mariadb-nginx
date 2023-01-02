0793094add38b267bab4109854c4149062d876af
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-02 09:07:05 +0000

    Update clean+create.sh

--- mariadb/clean+create.sh
+++ mariadb/clean+create.sh
@@ -1,5 +1,10 @@
 podman pod stop pod_mariadb_backend
 podman pod rm pod_mariadb_backend
+podman pod stop infra_mariadb_backend
+podman pod rm infra_mariadb_backend
+podman rm container_mariadb_backend
+podman rmi image_mariadb_1
+
 cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
 
 uid=1001
@@ -21,13 +26,13 @@ podman pod create \
 --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
 --publish 3306:3306
 
-podman build -t my_mariadb_1 .
+podman build -t image_mariadb_1 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
---name mariadb_backend_1 \
+--name container_mariadb_backend \
 --pod pod_mariadb_backend \
 --volume /opt/dmtools/mysql:/var/lib/mysql:z \
 --user $uid:$gid \
-localhost/my_mariadb_1:latest
+localhost/image_mariadb_1:latest
