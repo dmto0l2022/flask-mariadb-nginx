a05f31ba5a74308d0a592b1e116eab5cf9d22da8
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-03 10:02:08 +0000

    Update session_app.py

--- flask_dash_frontend/app/dashapps/session_app.py
+++ flask_dash_frontend/app/dashapps/session_app.py
@@ -12,6 +12,17 @@ server = flask.Flask(__name__)
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],server=server, requests_pathname_prefix='/session_app/')
 
+import os
+from os import environ, path
+
+from dotenv import load_dotenv
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
+
+print('BASE_DIR_DASHAPP')
+print(BASE_DIR)
+
 app.layout = html.Div([
             html.H1(children="Hello world!",className="hello",id="div3",
     style={'color':'#00361c','text-align':'center'
