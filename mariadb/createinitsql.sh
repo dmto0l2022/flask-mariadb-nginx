542bd2d41f6790d574204ef15203606c4614495c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-11 13:07:54 +0000

    Update createinitsql.sh

--- mariadb/createinitsql.sh
+++ mariadb/createinitsql.sh
@@ -1,14 +1,14 @@
 ##echo 'This is a test' > init_new.sql
 ##echo ${FIRST_NAME} > init_new.sql
 
-echo "CREATE USER 'pythonuser'@'localhost' IDENTIFIED BY 'pythonuser';" > init_new.sql
+echo "CREATE USER '${PYTHONUSER}'@'localhost' IDENTIFIED BY '${PYTHONUSERPASSWORD}';" > init_new.sql
 
 echo "CREATE DATABASE data;" >> init_new.sql
 
-echo "GRANT ALL PRIVILEGES ON *.* TO 'pythonuser'@'localhost' WITH GRANT OPTION;" >> init_new.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${PYTHONUSER}'@'localhost' WITH GRANT OPTION;" >> init_new.sql
 
-echo "ALTER USER 'pythonuser'@'%' IDENTIFIED BY 'pythonuser';" >> init_new.sql
+echo "ALTER USER '${PYTHONUSER}'@'%' IDENTIFIED BY '${PYTHONUSERPASSWORD}';" >> init_new.sql
 
-echo "GRANT ALL PRIVILEGES ON *.* TO 'pythonuser'@'%' WITH GRANT OPTION;" >> init_new.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${PYTHONUSER}'@'%' WITH GRANT OPTION;" >> init_new.sql
 
 ## This is required when using Dockerfile
