1c8ac3266700ae9b94e7f070fc54d124252817f6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-12 17:07:40 +0000

    Update main_build.sh

--- aa_build/main_build.sh
+++ aa_build/main_build.sh
@@ -1,1 +1,48 @@
+podman pod stop pod_mariadb_backend
+podman pod rm pod_mariadb_backend
+# podman pod stop infra_mariadb_backend
+# podman pod rm infra_mariadb_backend
+# podman rm container_mariadb_backend
+podman rmi image_mariadb_1
 
+cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
+
+uid=${ENV_UID} ##1001
+gid=${ENV_GID} ##1002
+
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman pod create \
+--name pod_mariadb_backend \
+--infra-name infra_mariadb_backend \
+--network bridge \
+--uidmap 0:1:$uid \
+--uidmap $uid:0:1 \
+--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+--gidmap 0:1:$gid \
+--gidmap $gid:0:1 \
+--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--publish 3306:3306
+
+podman build \
+--build-arg=ENV_UID=${ENV_UID} \
+--build-arg=ENV_USERNAME=${ENV_USERNAME} \
+--build-arg=ENV_GID=${ENV_GID} \
+--build-arg=ENV_GROUPNAME=${ENV_GROUPNAME} \
+--build-arg=ENV_MARIADB_USER=${ENV_MARIADB_USER} \
+--build-arg=ENV_MARIADB_PASSWORD=${ENV_MARIADB_PASSWORD} \
+--build-arg=ENV_MARIADB_ROOT_PASSWORD=${ENV_MARIADB_ROOT_PASSWORD} \
+--build-arg=ENV_MARIADB_DATABASE=${ENV_MARIADB_DATABASE} \
+-t image_mariadb_1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_mariadb_backend \
+--pod pod_mariadb_backend \
+--volume /opt/dmtools/mysql:/var/lib/mysql:z \
+--user $uid:$gid \
+localhost/image_mariadb_1:latest
