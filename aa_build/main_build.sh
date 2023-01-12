25ab8e6df13973576774ae9c0746f2eb442a3c70
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-12 17:18:40 +0000

    Update main_build.sh

--- aa_build/main_build.sh
+++ aa_build/main_build.sh
@@ -1,8 +1,6 @@
-podman pod stop pod_mariadb_backend
-podman pod rm pod_mariadb_backend
-# podman pod stop infra_mariadb_backend
-# podman pod rm infra_mariadb_backend
-# podman rm container_mariadb_backend
+podman pod stop pod_main_backend
+podman pod rm pod_main_backend
+
 podman rmi image_mariadb_1
 
 cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
@@ -16,8 +14,8 @@ subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
 podman pod create \
---name pod_mariadb_backend \
---infra-name infra_mariadb_backend \
+--name pod_main_backend \
+--infra-name infra_main_backend \
 --network bridge \
 --uidmap 0:1:$uid \
 --uidmap $uid:0:1 \
@@ -25,7 +23,7 @@ podman pod create \
 --gidmap 0:1:$gid \
 --gidmap $gid:0:1 \
 --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
---publish 3306:3306
+--publish 5000:5000
 
 podman build \
 --build-arg=ENV_UID=${ENV_UID} \
@@ -42,7 +40,7 @@ podman build \
 
 podman run -dt \
 --name container_mariadb_backend \
---pod pod_mariadb_backend \
+--pod pod_main_backend \
 --volume /opt/dmtools/mysql:/var/lib/mysql:z \
 --user $uid:$gid \
 localhost/image_mariadb_1:latest
@@ -50,35 +48,9 @@ localhost/image_mariadb_1:latest
 #####
 
 podman stop container_api_backend_1
-# podman stop infra_api_backend
-# podman rm container_api_backend_1
-#podman rm infra_api_backend
-
-podman pod stop pod_api_backend
-podman pod rm pod_api_backend
-
 podman rmi image_pythonapi_1
 
 cd /opt/dmtools/code/flask-mariadb-nginx/flask_crud_api
-
-uid=1001
-gid=1002
-subuidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
-subgidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
-
-podman pod create \
---name pod_api_backend \
---infra-name infra_api_backend \
---network bridge \
---uidmap 0:1:$uid \
---uidmap $uid:0:1 \
---uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
---gidmap 0:1:$gid \
---gidmap $gid:0:1 \
---gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
---publish 8004:8004
 
 podman build -f Dockerfile_pythonbase -t image_pythonbaseapi_1 .
 podman build -f Dockerfile_pythonapi -t image_pythonapi_1 .
@@ -87,37 +59,16 @@ podman build -f Dockerfile_pythonapi -t image_pythonapi_1 .
 
 podman run -dt \
 --name container_api_backend_1 \
---pod pod_api_backend \
+--pod pod_main_backend \
 --user $uid:$gid \
 localhost/image_pythonapi_1:latest
 
 ####
 
 podman stop container_flask_dash_frontend_1
-podman pod stop pod_flask_dash_frontend
-podman pod rm pod_flask_dash_frontend
 podman rmi image_flask_dash_frontend_1
 
 cd /opt/dmtools/code/flask-mariadb-nginx/flask_dash_frontend
-
-uid=1001
-gid=1002
-subuidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
-subgidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
-
-podman pod create \
---name pod_flask_dash_frontend \
---infra-name infra_flask_dash_frontend \
---network bridge \
---uidmap 0:1:$uid \
---uidmap $uid:0:1 \
---uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
---gidmap 0:1:$gid \
---gidmap $gid:0:1 \
---gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
---publish 8002:8002
 
 podman build -f Dockerfile_pythonfrontendbase -t image_python_base_frontend_1 .
 podman build -f Dockerfile_pythonfrontend -t image_flask_dash_frontend_1 .
@@ -126,7 +77,7 @@ podman build -f Dockerfile_pythonfrontend -t image_flask_dash_frontend_1 .
 
 podman run -dt \
 --name container_flask_dash_frontend_1 \
---pod pod_flask_dash_frontend \
+--pod pod_main_backend \
 --user $uid:$gid \
 localhost/image_flask_dash_frontend_1:latest
 
