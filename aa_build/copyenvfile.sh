dc62a23cde1dcdfe96929eb1831192845e7b06dd
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-12 09:25:20 +0000

    Update copyenvfile.sh

--- aa_build/copyenvfile.sh
+++ aa_build/copyenvfile.sh
@@ -1,5 +1,9 @@
 #cp ./.env ../
+# remember to paste the .env file into aa_build before running this
 
 cp ./.env ../mariadb/.env
 cp ./.env ../flask_crud_api/app/.env
 cp ./.env ../flask_dash_frontend/.env
+## set the variables for the mariadb 
+source ../mariadb/setenv.sh
+source ../mariadb/createsql.sh
