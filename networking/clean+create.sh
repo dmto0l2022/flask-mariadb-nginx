732fe97c8ab43bd95997c973d072b0037e2c3b9c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-12 09:58:30 +0000

    Create clean+create.sh

---
+++ networking/clean+create.sh
@@ -0,0 +1,26 @@
+podman stop container_1
+podman stop container_2
+
+podman pod rm pod_main
+podman rmi nginx:latest
+
+podman pod create \
+--name pod_main \
+--infra-name infra_1 \
+--network bridge \
+--publish 80:80
+
+podman pull nginx:latest
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_1 \
+--pod pod_main \
+localhost/nginx:latest
+
+podman run -dt \
+--name container_2 \
+--pod pod_main \
+localhost/nginx:latest
+
