b6ee21711b238785707b506130c299d2c3ce08ed
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-12 16:12:33 +0000

    Update clean+create443.sh

--- networking/clean+create443.sh
+++ networking/clean+create443.sh
@@ -31,8 +31,18 @@ podman run -dt \
 --pod pod_main \
 localhost/image_nginx8090:latest
 
+## host:container
+
 podman run -dt \
 --name container_443 \
 --pod pod_main \
--v /etc/letsencrypt:/etc/letsencrypt \
+-v /etc/pki/nginx:/etc/pki/nginx \
+-v /var/www/phetwebcit.services.brown.edu:/var/www \
+-v /var/log/nginx/phetwebcit.services.brown.edu:/var/log/nginx/log \
 localhost/image_nginx443:latest
+
+#    root         /var/www/phetwebcit.services.brown.edu/:/var/www/
+#    access_log   /var/log/nginx/phetwebcit.services.brown.edu/access.log;
+#    error_log    /var/log/nginx/phetwebcit.services.brown.edu/error.log;
+#    ssl_certificate "/etc/pki/nginx/dmtools_het_brown_edu.pem";
+#    ssl_certificate_key "/etc/pki/nginx/private/dmtools.key";
