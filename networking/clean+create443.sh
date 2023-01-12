8daf4bdd803f8d78c85c97582b405621b6cb62e2
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-12 15:45:56 +0000

    Update clean+create443.sh

--- networking/clean+create443.sh
+++ networking/clean+create443.sh
@@ -1,1 +1,38 @@
-podman run -dt --pod new:frontend -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt localhost/my-nginx-1:latest
+podman stop container_8080
+podman stop container_8090
+podman stop container_443
+
+podman pod rm pod_main
+podman rmi nginx:latest
+podman rmi image_nginx443:latest
+podman rmi image_nginx8080:latest
+podman rmi image_nginx8090:latest
+
+
+podman pod create \
+--name pod_main \
+--infra-name infra_1 \
+--network bridge \
+--publish 443:443
+
+podman build -f Dockerfile443 -t image_nginx443 .
+podman build -f Dockerfile8080 -t image_nginx8080 .
+podman build -f Dockerfile8090 -t image_nginx8090 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_8080 \
+--pod pod_main \
+localhost/image_nginx8080:latest
+
+podman run -dt \
+--name container_8090 \
+--pod pod_main \
+localhost/image_nginx8090:latest
+
+podman run -dt \
+--name container_443 \
+--pod pod_main \
+-v /etc/letsencrypt:/etc/letsencrypt
+localhost/image_nginx443:latest
