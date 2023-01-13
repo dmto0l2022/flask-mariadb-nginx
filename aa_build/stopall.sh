44ed7f619af76439f83a97ac249811fc991f24c9
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-13 09:07:42 +0000

    Update stopall.sh

--- aa_build/stopall.sh
+++ aa_build/stopall.sh
@@ -3,7 +3,4 @@ podman stop container_api_backend_1
 podman stop container_mariadb_backend
 podman stop container_redis_1
 
-podman stop infra_mariadb_backend
-podman stop infra_api_backend
-podman stop infra_flask_dash_frontend
-podman stop infra_redis
+
