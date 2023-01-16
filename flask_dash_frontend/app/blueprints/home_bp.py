7243c52f476f891c6312d435ff872613768bd9e8
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-16 17:01:14 +0000

    Update home_bp.py

--- flask_dash_frontend/app/blueprints/home_bp.py
+++ flask_dash_frontend/app/blueprints/home_bp.py
@@ -2,10 +2,10 @@ from flask import Blueprint, render_template
 
 home_bp = Blueprint('home_bp', __name__)
 
-@home_bp.route('/', methods=['GET', 'POST'])
+@home_bp.route('/app/', methods=['GET', 'POST'])
 def rootofserver():
     return 'root'
 
-@home_bp.route('/home')
+@home_bp.route('/app/home')
 def home():
     return render_template('home.html')
