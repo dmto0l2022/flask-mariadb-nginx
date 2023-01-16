953bd99ebe0d0e65663d75c18b8dd4bba66e9085
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-16 15:50:34 +0000

    Update make+pushimages.sh

--- aa_build/make+pushimages.sh
+++ aa_build/make+pushimages.sh
@@ -6,8 +6,8 @@
 cd /opt/dmtools/code/flask-mariadb-nginx/redis
 podman rmi redis_1:latest
 podman build -f Dockerfile -t redis_1 .
-podman tag redis_1:latest dmto0l2022/redis_1:latest
-podman push dmto0l2022/redis_1:latest
+#podman tag redis_1:latest dmto0l2022/redis_1:latest
+podman push redis_1:latest dmto0l2022/redis_1:latest
 
 cd /opt/dmtools/code/flask-mariadb-nginx/mariadb
 podman rmi mariadb_1
@@ -31,7 +31,8 @@ podman rmi python_api_1
 cd /opt/dmtools/code/flask-mariadb-nginx/flask_crud_api
 
 podman build -f Dockerfile_pythonbase -t python_base_api_1 .
-podman tag python_base_api_1:latest dmto0l2022/python_base_api_1:latest
+#podman tag python_base_api_1:latest dmto0l2022/python_base_api_1:latest
+#podman push dmto0l2022/python_base_api_1:latest
 podman push dmto0l2022/python_base_api_1:latest
 
 ## this is only local as it contains the env file
@@ -46,7 +47,7 @@ podman rmi flask_dash_frontend_1
 cd /opt/dmtools/code/flask-mariadb-nginx/flask_dash_frontend
 
 podman build -f Dockerfile_pythonfrontendbase -t python_base_frontend_1 .
-podman tag python_base_frontend_1:latest dmto0l2022/python_base_frontend_1:latest
+#podman tag python_base_frontend_1:latest dmto0l2022/python_base_frontend_1:latest
 podman push dmto0l2022/python_base_frontend_1:latest
 
 ## this is only local as it contains the env file
