1c88d946002a381b25e2677ec0efedcd219a7732
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-16 16:36:25 +0000

    Create rebuildfront.sh

---
+++ flask_dash_frontend/rebuildfront.sh
@@ -0,0 +1,10 @@
+podman build -f Dockerfile_pythonfrontend -t image_flask_dash_frontend_1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_flask_dash_frontend_1 \
+--pod pod_flask_dash_frontend \
+--user $uid:$gid \
+localhost/image_flask_dash_frontend_1:latest
+
