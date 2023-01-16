2aaaf46defd4336cdd83fd9f314b0c50fc89b931
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-16 12:20:03 +0000

    Create clean+create443dev.sh

---
+++ networking/clean+create443dev.sh
@@ -0,0 +1,43 @@
+podman stop container_8080
+podman stop container_8090
+podman stop container_443
+
+podman pod rm pod_main
+podman rmi nginx:latest
+podman rmi image_nginx443:latest
+podman rmi image_nginx8080:latest
+podman rmi image_nginx8090:latest
+
+
+podman pod create \
+--name pod_main \
+--infra-name infra_1 \
+--network bridge \
+--publish 443:443 \
+--publish 80:80
+
+podman build -f Dockerfile443dev -t image_nginx443 .
+podman build -f Dockerfile8080 -t image_nginx8080 .
+podman build -f Dockerfile8090 -t image_nginx8090 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_8080 \
+--pod pod_main \
+localhost/image_nginx8080:latest
+
+podman run -dt \
+--name container_8090 \
+--pod pod_main \
+localhost/image_nginx8090:latest
+
+## host:container
+
+podman run -dt \
+--name container_443 \
+--pod pod_main \
+-v /etc/pki/nginx:/etc/pki/nginx \
+-v /var/www/phetwebcit.services.brown.edu:/var/www \
+-v /var/log/nginx/phetwebcit.services.brown.edu:/var/log/nginx/log \
+localhost/image_nginx443:latest
