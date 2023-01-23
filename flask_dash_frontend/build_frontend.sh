24f098834efa51941166fbc50a7172d960066f93
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-23 15:22:08 +0000

    Update build_frontend.sh

--- flask_dash_frontend/build_frontend.sh
+++ flask_dash_frontend/build_frontend.sh
@@ -1,3 +1,11 @@
+uid=${ENV_UID} ##1001
+gid=${ENV_GID} ##1002
+
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
 #podman build -f Dockerfile_pythonfrontendbase -t image_python_base_frontend_1 .
 podman rmi flask_dash_frontend_1:latest
 podman build -f Dockerfile_pythonfrontend -t flask_dash_frontend_1 .
