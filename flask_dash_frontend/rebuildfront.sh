259c6204910aa7c6ebfb66cbe7d91a1a04eaaf5d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-23 15:23:55 +0000

    Update rebuildfront.sh

--- flask_dash_frontend/rebuildfront.sh
+++ flask_dash_frontend/rebuildfront.sh
@@ -1,3 +1,12 @@
+id=${ENV_UID} ##1001
+gid=${ENV_GID} ##1002
+
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+
 podman stop container_flask_dash_frontend_1
 podman rm container_flask_dash_frontend_1
 podman rmi image_flask_dash_frontend_1
