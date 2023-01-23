1e58276bd864bc3390a0f2b33dd96053cf5a4d4e
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-23 17:36:05 +0000

    Create recreate.sh

---
+++ redis/recreate.sh
@@ -0,0 +1,22 @@
+id=${ENV_UID} ##1001
+gid=${ENV_GID} ##1002
+
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+
+podman stop container_redis_1
+podman rm container_redis_1
+podman rmi redis_1
+
+podman build -f Dockerfile -t redis_1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_redis_1 \
+--pod pod_main_backend \
+--user $uid:$gid \
+localhost/redis_1:latest
