cec328435cfc37e9d6161e270f2b9ef59ceff6e4
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-24 15:25:13 +0000

    Update wsgi.py

--- flask_dash_frontend/wsgi.py
+++ flask_dash_frontend/wsgi.py
@@ -16,3 +16,18 @@ application = DispatcherMiddleware(app, {
     '/session_app': app3.server,
     '/multipage': app4.server,
 })  
+
+class Middleware(app):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        # not Flask request - from werkzeug.wrappers import Request
+        request = Request(environ)
+        print('path: %s, url: %s' % (request.path, request.url))
+        # just do here everything what you need
+        return self.app(environ, start_response)
+
+
+application = Middleware(application)
