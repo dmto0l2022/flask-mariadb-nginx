d94312faa91f588b9ee08d1928c7ac9a0827934a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-24 10:36:21 +0000

    Update auth_bp.py

--- flask_dash_frontend/app/blueprints/auth_bp.py
+++ flask_dash_frontend/app/blueprints/auth_bp.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from current_app import db
+from app import db
 
 auth_bp = Blueprint('auth_bp', __name__, url_prefix='/app/auth')
 
