9ce62adbdd43a06441837887ecf22e934e7c94e3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-24 08:11:24 +0000

    Update dashapp1_bp.py

--- flask_dash_frontend/app/blueprints/dashapp1_bp.py
+++ flask_dash_frontend/app/blueprints/dashapp1_bp.py
@@ -1,15 +1,22 @@
 from flask import Blueprint, render_template, redirect
 
+from flask_security import Security, SQLAlchemyUserDatastore, auth_required, hash_password
+
+from flask_security.models import fsqla_v3 as fsqla
+
+from flask_login import current_user
+
 dashapp1_bp = Blueprint('dashapp1_bp', __name__)
 
-@dashapp1_bp.route('/dashapp1')
+@dashapp1_bp.route('/app/dashapp1')
 def dashapp1():
     return redirect('/wsgi_app1', code=302)
 
-@dashapp1_bp.route('/dashapp2')
+@dashapp1_bp.route('/app/dashapp2')
+@auth_required()
 def dashapp2():
     return redirect('/wsgi_app2', code=302)
 
-@dashapp1_bp.route('/session_app1')
+@dashapp1_bp.route('/app/session_app1')
 def dashapp3():
     return redirect('/session_app', code=302)
