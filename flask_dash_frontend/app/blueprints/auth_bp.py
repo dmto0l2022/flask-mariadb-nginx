4deaa7f9ba675a2b52381e8d854f5f7e9c807c84
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-25 11:05:13 +0000

    Update auth_bp.py

--- flask_dash_frontend/app/blueprints/auth_bp.py
+++ flask_dash_frontend/app/blueprints/auth_bp.py
@@ -22,58 +22,30 @@ def load_logged_in_user():
 '''
 
 @auth_bp.route('/app/auth/logout')
+@login_required
 def logout():
-    session.clear()
-    return redirect(url_for('index'))         
+    logout_user()
+    return redirect(somewhere)     
   
-@auth_bp.route('/app/auth/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        error = None
-
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template('auth/register.html')
   
 @auth_bp.route('/app/auth/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+def login_auth():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
 
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+        flask.flash('Logged in successfully.')
 
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
 
-        flash(error)
-
-    return render_template('auth/login.html')
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
