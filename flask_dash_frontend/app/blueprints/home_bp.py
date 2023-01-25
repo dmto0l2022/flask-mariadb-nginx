f88c78b579165819fa658f5d67ab76f57b1dd779
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-25 07:06:07 +0000

    Update home_bp.py

--- flask_dash_frontend/app/blueprints/home_bp.py
+++ flask_dash_frontend/app/blueprints/home_bp.py
@@ -39,7 +39,7 @@ def homepage():
     return """
     <h1>Hello world!</h1>
 
-    <iframe src="http://container_flask_dash_frontend_1:8002/wsgi_app2'" width="853" height="480" frameborder="0" allowfullscreen></iframe>
+    <iframe src="http://0.0.0.0:8002/wsgi_app2'" width="853" height="480" frameborder="0" allowfullscreen></iframe>
     """
 
 '''
